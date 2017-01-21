8e28c648846e9526b661a32509261f0cbc40d8f5
traderain <hambalko.bence@gmail.com>
2017-01-21 22:03:26 +0100

    Add quest design form

---
+++ W3Edit/SaveModels/SavegameModel.cs
@@ -0,0 +1,13 @@
+﻿namespace W3SavegameEditor.Models
+{
+    public class SavegameModel
+    {
+        public string Name { get; set; }
+
+        public string Path { get; set; }
+
+        public string ThumbnailPath { get; set; }
+
+        public SavegameDataModel Data { get; set; }
+    }
+}
