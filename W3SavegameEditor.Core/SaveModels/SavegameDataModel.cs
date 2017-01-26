c7f468f6101fc29a2ba23e09f6569dd51913f1d5
traderain <hambalko.bence@gmail.com>
2017-01-26 01:01:51 +0100

    Fixed SaveGame files

---
+++ W3SavegameEditor.Core/SaveModels/SavegameDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+
+namespace W3SavegameEditor.Models
+{
+    public class SavegameDataModel
+    {
+        public int Version1 { get; set; }
+        public int Version2 { get; set; }
+        public int Version3 { get; set; }
+
+        public ObservableCollection<string> VariableNames { get; set; }
+
+        public ObservableCollection<VariableModel> Variables { get; set; }
+    }
+}
