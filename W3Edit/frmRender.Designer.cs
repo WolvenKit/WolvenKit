c29f61923fa7d985fb094b901c8a0e8541027ce0
traderain <hambalko.bence@gmail.com>
2017-01-28 23:56:47 +0100

    Add GLControl and setup frmRender

--- W3Edit/frmRender.Designer.cs
+++ W3Edit/frmRender.Designer.cs
@@ -28,11 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.glControl1 = new OpenTK.GLControl();
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.glControl1.Location = new System.Drawing.Point(0, 0);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(735, 548);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            // 
+            // frmRender
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "frmRender";
+            this.ClientSize = new System.Drawing.Size(735, 548);
+            this.Controls.Add(this.glControl1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmRender";
+            this.ShowIcon = false;
+            this.Text = "Render window";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private OpenTK.GLControl glControl1;
     }
 }
