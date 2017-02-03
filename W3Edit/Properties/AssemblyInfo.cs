132fbdd6a1ddae7222bec2c552d01b494927506b
traderain <hambalko.bence@gmail.com>
2017-02-03 15:24:18 +0100

    Update version to 0.04

--- W3Edit/Properties/AssemblyInfo.cs
+++ W3Edit/Properties/AssemblyInfo.cs
@@ -43,5 +43,5 @@ Awesome people:
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.0.3.*")]
-[assembly: AssemblyFileVersion("0.0.3.0")]
+[assembly: AssemblyVersion("0.0.4.*")]
+[assembly: AssemblyFileVersion("0.0.4.0")]
