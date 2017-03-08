49f7ec87c4ad5a249572dfe4b9460ae6beddd5cc
traderain <hambalko.bence@gmail.com>
2017-03-08 21:50:30 +0100

    Output context menu
    - Copy
    - Save as

--- W3Edit/frmOutput.Designer.cs
+++ W3Edit/frmOutput.Designer.cs
@@ -31,29 +31,59 @@ namespace W3Edit
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmOutput));
             this.txOutput = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // txOutput
             // 
+            this.txOutput.ContextMenuStrip = this.contextMenuStrip1;
             this.txOutput.Dock = System.Windows.Forms.DockStyle.Fill;
             this.txOutput.Location = new System.Drawing.Point(0, 0);
             this.txOutput.Name = "txOutput";
-            this.txOutput.Size = new System.Drawing.Size(284, 262);
+            this.txOutput.Size = new System.Drawing.Size(930, 259);
             this.txOutput.TabIndex = 0;
             this.txOutput.Text = "";
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.saveAsToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(140, 56);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(139, 26);
+            this.copyToolStripMenuItem.Text = "Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(139, 26);
+            this.saveAsToolStripMenuItem.Text = "Save as..";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
             // frmOutput
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(930, 259);
             this.Controls.Add(this.txOutput);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmOutput";
             this.Text = "Output";
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -61,5 +91,8 @@ namespace W3Edit
         #endregion
 
         private RichTextBox txOutput;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem copyToolStripMenuItem;
+        private ToolStripMenuItem saveAsToolStripMenuItem;
     }
 }
--- W3Edit/frmOutput.cs
+++ W3Edit/frmOutput.cs
@@ -1,4 +1,6 @@
-﻿using WeifenLuo.WinFormsUI.Docking;
+﻿using System.IO;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
 
 namespace W3Edit
 {
@@ -18,6 +20,24 @@ namespace W3Edit
         internal void Clear()
         {
             txOutput.Clear();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            Clipboard.SetText(txOutput.Text);
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            using (var sf = new SaveFileDialog())
+            {
+                sf.Title = "Choose a location to save the log to.";
+                sf.Filter = "Text file (.txt) | *.txt";
+                if (sf.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllLines(sf.FileName,txOutput.Lines);
+                }
+            }
         }
     }
 }
