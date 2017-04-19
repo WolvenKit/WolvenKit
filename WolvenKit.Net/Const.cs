fecff29c01e457834de1ca8bbd883d2858be9afa
traderain <hambalko.bence@gmail.com>
2017-04-19 15:25:44 +0200

    Add network constants and game connection

---
+++ WolvenKit.Net/Const.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WolvenKit.Net
+{
+    public static class Const
+    {
+        public static string NsScriptDebugger   = "ScriptDebugger";
+        public static string NsScriptProfiler   = "ScriptProfiler";
+        public static string NsScriptCompiler   = "ScriptCompiler";
+        public static string NsScripts           = "scripts";
+        public static string NsRemote            = "Remote";
+        public static string NsUtility           = "Utility";
+        public static string NsConfig            = "Config";
+
+        public static string CmdBind = "BIND";
+        
+        public static byte[] PacketHead          = {0xDE, 0xAD}; // DEAD
+        public static byte[] PacketTail          = {0xBE, 0xEF}; // BEEF
+        
+        public static string ScRootPath = "RootPath";
+
+        public static string SdUnfilteredLocals = "UnfilteredLocals";
+        public static string SdSortLocals = "SortLocals";
+        public static string SdOpcodeRequest = "OpcodeBreakdownRequest";
+
+        public static string SReload = "reload";
+        public static string SPkgSync = "pkgSync";
+
+        public static string CfgList = "list";
+
+        public static byte[] TypeByte = {0x81, 0x08};
+        public static byte[] TypeStringUtf8 = {0xAC, 0x08, 0x81, 0x16};
+        public static byte[] TypeStringUtf16 = {0x9C, 0x16, 0x81, 0x16};
+        public static byte[] TypeInt32 = {0x81, 0x32};
+        public static byte[] TypeInt64 = {0x81, 0x64};
+        public static byte[] TypeUint32 = {0x71, 0x32};
+    }
+}
--- WolvenKit.Net/Program.cs
+++ WolvenKit.Net/Program.cs
@@ -3,13 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace WolvenKit.Net
 {
     class Program
     {
+        public static Socket GameSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        public static readonly IPEndPoint GameAdress = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 37001);
+
         static void Main(string[] args)
         {
+            var tries = 0;
+            Console.Title = "Witcher 3 Debug Protocol test";
+            for (;;)
+            {
+                Console.WriteLine($"[{tries}] Connecting to the game.");
+                Thread.Sleep(500);
+                Console.Clear();
+                Console.WriteLine($"[{tries}] Connecting to the game..");
+                Thread.Sleep(500);
+                Console.Clear();
+                Console.WriteLine($"[{tries}] Connecting to the game...");
+                Thread.Sleep(500);
+                if (ConnectToGame())
+                    break;
+                tries++;
+                Console.WriteLine("Couldn't connect to the game!\nPlease launch \"The Witcher 3: The Wild Hunt\" with the -net command line parameter!");
+                Thread.Sleep(1000);
+                Console.Clear();
+            }   
+            Console.Clear();
+            Console.WriteLine("Connected to the game!");
+            Console.Title = "Witcher 3 Debug Protocol test - [Connected]";
+            //TODO: Async loop and response parsing
+
+
+
+            Console.ReadLine();
+        }
+
+        public static bool ConnectToGame()
+        {
+            GameSocket.Connect(GameAdress);
+            return GameSocket.Connected;
         }
     }
 }
