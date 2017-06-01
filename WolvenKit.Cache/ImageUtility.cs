c47f240a820635d0a607a58dbf84222a435e3c69
Ákos Köte <kote2ster@gmail.com>
2017-06-01 19:00:15 +0200

    Fixed DDS header

--- WolvenKit.Cache/ImageUtility.cs
+++ WolvenKit.Cache/ImageUtility.cs
@@ -2,8 +2,10 @@
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Windows.Forms;
 using WolvenKit.CR2W;
 using WolvenKit.CR2W.Types;
+using W3Edit.Textures;
 
 namespace WolvenKit.Cache
 {
@@ -19,62 +21,55 @@ namespace WolvenKit.Cache
             {
                 var image = ((CBitmapTexture)(imagechunk.data)).Image;
                 var compression = imagechunk.GetVariableByName("compression").ToString();
-                var width = int.Parse(imagechunk.GetVariableByName("width").ToString());
-                var height = int.Parse(imagechunk.GetVariableByName("height").ToString());
+                var width = uint.Parse(imagechunk.GetVariableByName("width").ToString());
+                var height = uint.Parse(imagechunk.GetVariableByName("height").ToString());
+                var mips = imagechunk.GetVariableByName("residentMipIndex")!=null ? uint.Parse(imagechunk.GetVariableByName("residentMipIndex").ToString()) : 0;
                 var tempfile = new MemoryStream();
-                /* DDS magic header contains:
-                 *      Required .dds flags for compressed texture without pitch
-                 * More on: https://msdn.microsoft.com/en-us/library/windows/desktop/bb943982(v=vs.85).aspx */
-                byte[] ddsheader = 
-                {
-                    0x44, 0x44, 0x53, 0x20, 0x7C, 0x00, 0x00, 0x00, 0x07, 0x10, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00,
-                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
-                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00,
-                    0x05, 0x00, 0x00, 0x00, 0x44, 0x58, 0x54, 0x31, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x10, 0x40, 0x00,
-                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-                };
-                byte[] dxt;
+
+                var dxt = DDSHeader.Format_DXT1;
                 switch (compression)
                 {
                     case "TCM_DXTNoAlpha":
-                        dxt = new byte[] { 0x44, 0x58, 0x54, 0x31 };
+                        dxt = DDSHeader.Format_DXT1;
                         break;
                     case "TCM_DXTAlpha":
-                        dxt = new byte[] { 0x44, 0x58, 0x54, 0x35 };
+                        dxt = DDSHeader.Format_DXT5;
                         break;
                     case "TCM_NormalsHigh":
-                        dxt = new byte[] { 0x44, 0x58, 0x54, 0x35 };
+                        dxt = DDSHeader.Format_DXT5;
                         break;
                     case "TCM_Normals":
-                        dxt = new byte[] { 0x44, 0x58, 0x54, 0x31 };
+                        dxt = DDSHeader.Format_DXT1;
+                        break;
+                    case "TCM_NormalsGloss":
+                        dxt = DDSHeader.Format_DXT5;
+                        break;
+                    case "TCM_QualityControl":
+                        dxt = DDSHeader.Format_DXT5;
                         break;
                     default:
                         throw new Exception("Invalid compression type! [" + compression + "]");
                 }
+
+                DDSHeader ddsheader = new DDSHeader();
                 using (var bw = new BinaryWriter(tempfile))
                 {
-                    bw.Write(ddsheader);
-                    bw.Seek(0xC, SeekOrigin.Begin);
-                    bw.Write(height);
-                    bw.Seek(0x10, SeekOrigin.Begin);
-                    bw.Write(width);
-                    bw.Seek(0x54, SeekOrigin.Begin);
-                    bw.Write(dxt);
-                    bw.Seek(128, SeekOrigin.Begin);
-                    bw.Write(image.Bytes);
+                    bw.Write(ddsheader.generate(width, height, mips, dxt).ToArray());
+                    //bw.Write(image.Bytes);  // First 20 bytes is garbage
+                    bw.Write( (new ArraySegment<byte>(image.Bytes, 20, image.Bytes.Length - 20)).ToArray() );
                 }
                 tempfile.Flush();
 #if DEBUG
-                File.WriteAllBytes(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\asd.dds",tempfile.GetBuffer());
+                //File.WriteAllBytes(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\asd.dds",tempfile.GetBuffer());
 #endif
                 return new DdsImage(tempfile.GetBuffer()).BitmapImage;
             }
-            catch(Exception)
+            catch(Exception e)
             {
-                // fails silently...
+                string message = e.Message;
+                string caption = "Error!";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                MessageBox.Show(message, caption, buttons);
                 return null;
             }
             
