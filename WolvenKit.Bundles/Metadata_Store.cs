34514ffb3c68c5547610ad6b09bd46ccc0b08812
Traderain <hambalko.bence@gmail.com>
2017-07-17 15:32:16 +0200

    Cleanup

--- WolvenKit.Bundles/Metadata_Store.cs
+++ WolvenKit.Bundles/Metadata_Store.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using WolvenKit.W3Strings;
 
 namespace WolvenKit.Bundles
 {
