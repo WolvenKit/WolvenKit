712aa30a815bccfbd69d4094ab009e3cd76e0c09
Traderain <hambalko.bence@gmail.com>
2017-07-25 10:53:38 +0200

    Fixed HSL Colorpicker for CR2W
    - #61

--- WolvenKit.CR2W/Types/CColorShift.cs
+++ WolvenKit.CR2W/Types/CColorShift.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
-using Cyotek.Windows.Forms;
 
 namespace WolvenKit.CR2W.Types
 {
     class CColorShift : CVector
     {
-        public HslColor Color = new HslColor(System.Drawing.Color.Black);
+        public HslColor Color = new HslColor(0,0,0);
 
         public CColorShift(CR2WFile cr2w) : base(cr2w) {  }
 
@@ -21,7 +20,7 @@ namespace WolvenKit.CR2W.Types
         {
             if (val is Color)
             {
-                Color = (Color)val;
+                Color = HslColor.FromRgb((Color)val);
             }
             return this;
         }
@@ -30,18 +29,18 @@ namespace WolvenKit.CR2W.Types
         {
             var panel = new Panel();
             if (((CUInt16) this.GetVariableByName("hue")) != null)
-                Color.H = ((CUInt16) this.GetVariableByName("hue")).val / 360;
+                Color.SetHue(((CUInt16) this.GetVariableByName("hue")).val);
             else
-                Color.H = 0;
+                Color.Hue = 0;
             if (((CInt8) this.GetVariableByName("saturation")) != null)
-                Color.S = ((CInt8) this.GetVariableByName("saturation")).val / 100;
+                Color.SetSaturation(((CInt8) this.GetVariableByName("saturation")).val);
             else
-                Color.S = 0;
+                Color.Saturation = 0;
             if (((CInt8) this.GetVariableByName("luminance")) != null)
-                Color.L = ((CInt8) this.GetVariableByName("luminance")).val / 100;
+                Color.SetLuminosity(((CInt8) this.GetVariableByName("luminance")).val);
             else
-                Color.L = 0;
-            panel.BackColor = Color;
+                Color.Luminosity = 0;
+            panel.BackColor = Color.ToRgb();
             panel.Click += panel_Click;
             panel.Height = 18;
             return panel;
@@ -49,26 +48,28 @@ namespace WolvenKit.CR2W.Types
 
         private void panel_Click(object sender, EventArgs e)
         {
-            var dlg = new Form();
-            dlg.Controls.Add(new ColorEditor()
+            using (var cd = new ColorDialog())
             {
-                Dock = DockStyle.Fill
-            });
-            ((ColorEditor)dlg.Controls[0]).HslColor = Color;
-            if(dlg.ShowDialog() == DialogResult.OK)
-            {
-                Color = ((ColorEditor)dlg.Controls[0]).HslColor;
-                if (((CUInt16)this.GetVariableByName("hue")) == null)
-                    cr2w.CreateVariable(this, "CUInt16", "hue");
-                ((CUInt16)this.GetVariableByName("hue")).val = (ushort)(Color.H * 360);
-                if (((CInt8)this.GetVariableByName("saturation")) == null)
-                    cr2w.CreateVariable(this, "CInt8", "saturation");
-                ((CInt8)this.GetVariableByName("saturation")).val = (sbyte)(Color.S * 100);
-                if (((CInt8)this.GetVariableByName("luminance")) == null)
-                    cr2w.CreateVariable(this, "CInt8", "luminance");
-                ((CInt8)this.GetVariableByName("luminance")).val = (sbyte)(Color.L * 100);
+                cd.Color = Color.ToRgb();
+                if (cd.ShowDialog() == DialogResult.OK)
+                {
+                    Color = HslColor.FromRgb(cd.Color);
+                    //TODO: Check if this can cause error! Sometimes the file breaks if we add the missing value. Needs more checks.
+                    /*if (((CUInt16)this.GetVariableByName("hue")) == null)
+                        cr2w.CreateVariable( "CUInt16", "hue");
+                    if (((CInt8)this.GetVariableByName("saturation")) == null)
+                        cr2w.CreateVariable("CInt8", "saturation");
+                    if (((CInt8)this.GetVariableByName("luminance")) == null)
+                        cr2w.CreateVariable("CInt8", "luminance");*/
+                    if (((CUInt16)this.GetVariableByName("hue")) != null)
+                        ((CUInt16)this.GetVariableByName("hue")).val = (ushort)Color.GetHue();
+                    if (((CInt8)this.GetVariableByName("saturation")) != null)
+                        ((CInt8)this.GetVariableByName("saturation")).val = (sbyte)Color.GetSaturation();
+                    if (((CInt8)this.GetVariableByName("luminance")) != null)
+                        ((CInt8)this.GetVariableByName("luminance")).val = (sbyte)Color.GetLuminosity();
+                }
+                ((Panel)sender).BackColor = Color.ToRgb();
             }
-            ((Panel)sender).BackColor = Color.ToRgbColor();
         }
     }
 }
