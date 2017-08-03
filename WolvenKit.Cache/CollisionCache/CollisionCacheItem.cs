e13ee5664147d116456cce71e7680a092cbbe1a1
traderain <hambalko.bence@gmail.com>
2017-08-03 21:49:27 +0200

    Added missing files

---
+++ WolvenKit.Cache/CollisionCache/CollisionCacheItem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ionic.Zlib;
+using WolvenKit.Interfaces;
+
+namespace WolvenKit.Cache.CollisionCache
+{
+    /// <summary>
+    /// Files packed into Collision.cache. Zlib compressed nxb/p3d file.
+    /// </summary>
+    public class CollisionCacheItem : IWitcherFile
+    {
+        public IWitcherArchiveType Bundle { get; set; }
+        public string Name { get; set; }
+        public long Size { get; set; }
+        public uint ZSize { get; set; }
+        public long Offset { get; set; }
+        public string CompressionType => "Zlib";
+
+        public ulong Unk1; //NULL
+        public ulong NameOffset;
+        public ulong Unk2;
+        public ulong Unk3;
+        public ulong Unk4;
+        public ulong Unk5;
+        public ulong Unk6;
+        public ulong Comtype; //TODO: Investigate this.
+
+        public void Extract(Stream output)
+        {
+            using (var file = MemoryMappedFile.CreateFromFile(Bundle.FileName, FileMode.Open))
+            {
+                using (var viewstream = file.CreateViewStream(Offset, ZSize, MemoryMappedFileAccess.Read))
+                {
+                    var zlib = new ZlibStream(viewstream, CompressionMode.Decompress);
+                    zlib.CopyTo(output);
+                }
+            }
+        }
+
+        public void Extract(string filename)
+        {
+            using (var output = new FileStream(filename, FileMode.CreateNew, FileAccess.Write))
+            {
+                Extract(output);
+                output.Close();
+            }
+        }
+    }
+}
---
+++ WolvenKit.Cache/CollisionCache/Collision_Cache.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WolvenKit.Interfaces;
+
+namespace WolvenKit.Cache.CollisionCache
+{
+    public class CollisionCache : IWitcherArchiveType
+    {
+        public byte[] IdString = { (byte)'C', (byte)'C', (byte)'3', (byte)'W' };
+        public uint Version;
+        public uint Unknown1;
+        public uint Unknown2;
+        public uint InfoOffset;
+        public uint NumberOfFiles;
+        public uint NameTableOffset;
+        public uint NamesSize;
+        public ulong Buffersize;
+        public ulong CheckSum;
+
+        public List<string> FileNames;
+        public List<CollisionCacheItem> Files = new List<CollisionCacheItem>(); 
+
+        public string TypeName => "CollisionCache";
+        public string FileName { get; set; }
+
+        public CollisionCache(string filename)
+        {
+            this.Read(new BinaryReader(new FileStream(filename, FileMode.Open)));
+        }
+
+        public void Read(BinaryReader br)
+        {
+            if (!br.ReadBytes(4).SequenceEqual(IdString))
+                throw new Exception("Invalid file!");
+            this.Version = br.ReadUInt32();
+            this.Unknown1 = br.ReadUInt32();
+            this.Unknown2 = br.ReadUInt32();
+            this.InfoOffset = br.ReadUInt32();
+            this.NumberOfFiles = br.ReadUInt32();
+            this.NameTableOffset = br.ReadUInt32();
+            this.NamesSize = br.ReadUInt32();
+            this.Buffersize = br.ReadUInt64();
+            this.CheckSum = br.ReadUInt64();
+            FileNames = new List<string>();
+            br.BaseStream.Seek(this.NameTableOffset, SeekOrigin.Begin);
+            for (int i = 0; i < this.NumberOfFiles; i++)
+            {
+                this.FileNames.Add(br.ReadCR2WString());
+            }
+            foreach (var ci in FileNames.Select(fileName => new CollisionCacheItem
+            {
+                Name = fileName,
+                Bundle = this,
+                Unk1 = br.ReadUInt64(),
+                NameOffset = br.ReadUInt64(),
+                Offset = br.ReadUInt32(),
+                ZSize = br.ReadUInt32(),
+                Unk2 = br.ReadUInt64(),
+                Unk3 = br.ReadUInt64(),
+                Unk4 = br.ReadUInt64(),
+                Unk5 = br.ReadUInt64(),
+                Unk6 = br.ReadUInt64(),
+                Comtype = br.ReadUInt64()
+            }))
+            {
+                Files.Add(ci);
+            }
+        }
+    }
+}
