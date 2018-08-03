add7c3432697a799d3708a153e959af49f40eeca
Hambalkó Bence <hambalko.bence@gmail.com>
2018-08-03 13:41:35 +0200

    Fix wcc_lite license form

--- WolvenKit/Forms/frmWCCLicense.cs
+++ WolvenKit/Forms/frmWCCLicense.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Windows.Forms;
 using RtfPipe;
+using RtfPipe.Support;
 using WolvenKit.Properties;
 
 namespace WolvenKit
@@ -11,10 +12,7 @@ namespace WolvenKit
         public frmWCCLicense()
         {
             InitializeComponent();
-            using (var reader = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(Resources.wcc_eula))))
-            {
-                browserwcclicense.DocumentText = Rtf.ToHtml(new RtfSource(reader));
-            }
+            browserwcclicense.DocumentText = Rtf.ToHtml(new RtfSource(new MemoryStream(Encoding.UTF8.GetBytes(Resources.wcc_eula))));
         }
     }
 }
