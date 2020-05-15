2c0d9d3d25aa4da52cca7249654aeab9b7ddd725
rfuzzo <r.fuzzo@gmail.com>
2020-05-15 08:52:04 +0200

    bugfixes
    - fixed a bug where empty arrays would throw an exception on save
    - fixed a bug where opening files without a mod project would throw an exception
    - fixed a bug with dual monitor maximizing (more winapi nonsense)
    - fixed a bug with copying null pointers (lol)

--- WolvenKit/Forms/frmMain.cs
+++ WolvenKit/Forms/frmMain.cs
@@ -45,6 +45,7 @@ namespace WolvenKit
     using WolvenKit.Scaleform;
     using System.Text.RegularExpressions;
     using WolvenKit.Common.Model;
+    using System.Runtime.InteropServices;
 
     public partial class frmMain : Form
     {
@@ -81,13 +82,6 @@ namespace WolvenKit
 
         private delegate void strDelegate(string t);
         private delegate void logDelegate(string t, Logtype type);
-
-        public const int WM_NCLBUTTONDOWN = 0xA1;
-        public const int HTCAPTION = 0x2;
-        [System.Runtime.InteropServices.DllImport("USer32.dll")]
-        public static extern bool ReleaseCapture();
-        [System.Runtime.InteropServices.DllImport("USer32.dll")]
-        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
         #endregion
 
         #region Properties
@@ -141,16 +135,23 @@ namespace WolvenKit
             hotkeys.RegisterHotkey(Keys.Control | Keys.V, HKPaste, "Paste");
             MainController.InitForm(this);
 
-            
-            
 
-            Screen screen = Screen.FromControl(this);
-            int x = screen.WorkingArea.X - screen.Bounds.X;
-            int y = screen.WorkingArea.Y - screen.Bounds.Y;
-            this.MaximizedBounds = new Rectangle(x, y,
-                screen.WorkingArea.Width, screen.WorkingArea.Height);
+            //SourceInitialized += (s, e) =>
+            //{
+            //    IntPtr handle = (new WindowInteropHelper(this)).Handle;
+            //    HwndSource.FromHwnd(handle).AddHook(new HwndSourceHook(WindowProc));
+            //};
+
+            //Screen screen = Screen.FromControl(this);
+            //int x = screen.WorkingArea.X - screen.Bounds.X;
+            //int y = screen.WorkingArea.Y - screen.Bounds.Y;
+            //this.MaximizedBounds = new Rectangle(x, y,
+            //    screen.WorkingArea.Width, screen.WorkingArea.Height);
         }
         #endregion
+
+
+
 
         #region Methods
         public void GlobalApplyTheme()
@@ -173,42 +174,7 @@ namespace WolvenKit
             visualStudioToolStripExtender1.SetStyle(toolbarToolStrip, VisualStudioToolStripExtender.VsVersion.Vs2015, theme);
         }
 
-        #region UI formborderstyle none
-
-        private const long WS_SYSMENU = 0x00080000L;
-        private const long WS_BORDER = 0x00800000L;
-        private const long WS_SIZEBOX = 0x00040000L;
-        private const long WS_CHILD = 0x40000000L;
-        private const long WS_DLGFRAME = 0x00400000L;
-        private const long WS_MAXIMIZEBOX = 0x00010000L;
-        private const long WS_CAPTION = 0x00C00000L;
-
-        protected override CreateParams CreateParams
-        {
-            get
-            {
-                CreateParams cp = base.CreateParams;
-                {
-                    cp.Style |= (int)WS_BORDER;
-                    cp.Style |= (int)WS_SYSMENU;
-                    cp.Style |= (int)WS_SIZEBOX;                    
-                }
-                return cp;
-            }
-        }
-        private void MinimizeToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            WindowState = FormWindowState.Minimized;
-        }
-
-        private void RestoreToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            if (this.WindowState != FormWindowState.Maximized)
-                WindowState = FormWindowState.Maximized;
-            else
-                WindowState = FormWindowState.Normal;
-        }
-        #endregion
+        
 
         private void LoggerUpdated(object sender, PropertyChangedEventArgs e)
         {
@@ -1059,6 +1025,9 @@ namespace WolvenKit
 
         public void ScanAndRegisterCustomClasses()
         {
+            if (ActiveMod == null)
+                return;
+
             foreach (var wsfile in ActiveMod.ModFiles.Where(_ => Path.GetExtension(_) == ".ws"))
             {
                 string fullpath = Path.Combine(ActiveMod.ModDirectory, wsfile);
@@ -2909,6 +2878,174 @@ Would you like to open the problem steps recorder?", "Bug reporting", MessageBox
 
         #endregion // Mod Pack
 
+        #region UI formborderstyle none
+
+        private const long WS_SYSMENU = 0x00080000L;
+        private const long WS_BORDER = 0x00800000L;
+        private const long WS_SIZEBOX = 0x00040000L;
+        private const long WS_CHILD = 0x40000000L;
+        private const long WS_DLGFRAME = 0x00400000L;
+        private const long WS_MAXIMIZEBOX = 0x00010000L;
+        private const long WS_CAPTION = 0x00C00000L;
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HTCAPTION = 0x2;
         
+        [System.Runtime.InteropServices.DllImport("user32")]
+        public static extern bool ReleaseCapture();
+        [System.Runtime.InteropServices.DllImport("user32")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32")]
+        internal static extern bool GetMonitorInfo(IntPtr hMonitor, MONITORINFO lpmi);
+
+        [System.Runtime.InteropServices.DllImport("user32")]
+        internal static extern IntPtr MonitorFromWindow(IntPtr handle, int flags);
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                {
+                    cp.Style |= (int)WS_BORDER;
+                    cp.Style |= (int)WS_SYSMENU;
+                    cp.Style |= (int)WS_SIZEBOX;
+                }
+                return cp;
+            }
+        }
+        private void MinimizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void RestoreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Screen screen = Screen.FromControl(this);
+            //int x = screen.WorkingArea.X - screen.Bounds.X;
+            //int y = screen.WorkingArea.Y - screen.Bounds.Y;
+            //this.MaximizedBounds = new Rectangle(x, y,
+            //    screen.WorkingArea.Width, screen.WorkingArea.Height);
+
+            if (this.WindowState != FormWindowState.Maximized)
+                WindowState = FormWindowState.Maximized;
+            else
+                WindowState = FormWindowState.Normal;
+        }
+
+        // https://stackoverflow.com/a/42806834
+        protected override void WndProc(ref Message m)
+        {
+            Boolean handled = false;
+
+            switch (m.Msg)
+            {
+                case 0x0024:
+                    WmGetMinMaxInfo(m.HWnd, m.LParam);
+                    
+                    handled = true;
+                    break;
+            }
+            m.Result = IntPtr.Zero;
+            if (handled) DefWndProc(ref m); else base.WndProc(ref m);
+        }
+
+        private const int WINDOW_BORDER_BUFFER = 10;
+        private static void WmGetMinMaxInfo(IntPtr hwnd, IntPtr lParam)
+        {
+            MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
+            int MONITOR_DEFAULTTONEAREST = 0x00000002;
+            IntPtr monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+            if (monitor != IntPtr.Zero)
+            {
+                MONITORINFO monitorInfo = new MONITORINFO();
+                GetMonitorInfo(monitor, monitorInfo);
+                RECT rcWorkArea = monitorInfo.rcWork;
+                RECT rcMonitorArea = monitorInfo.rcMonitor;
+                mmi.ptMaxPosition.x = Math.Abs(rcWorkArea.left - rcMonitorArea.left) - WINDOW_BORDER_BUFFER;
+                mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top - rcMonitorArea.top) - WINDOW_BORDER_BUFFER;
+                mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left) + (2 * WINDOW_BORDER_BUFFER);
+                mmi.ptMaxSize.y = Math.Abs(rcWorkArea.bottom - rcWorkArea.top) + (2 * WINDOW_BORDER_BUFFER);
+            }
+            Marshal.StructureToPtr(mmi, lParam, true);
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            /// <summary>x coordinate of point.</summary>
+            public int x;
+            /// <summary>y coordinate of point.</summary>
+            public int y;
+            /// <summary>Construct a point of coordinates (x,y).</summary>
+            public POINT(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MINMAXINFO
+        {
+            public POINT ptReserved;
+            public POINT ptMaxSize;
+            public POINT ptMaxPosition;
+            public POINT ptMinTrackSize;
+            public POINT ptMaxTrackSize;
+        };
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public class MONITORINFO
+        {
+            public int cbSize = Marshal.SizeOf(typeof(MONITORINFO));
+            public RECT rcMonitor = new RECT();
+            public RECT rcWork = new RECT();
+            public int dwFlags = 0;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 0)]
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+            public static readonly RECT Empty = new RECT();
+            public int Width { get { return Math.Abs(right - left); } }
+            public int Height { get { return bottom - top; } }
+            public RECT(int left, int top, int right, int bottom)
+            {
+                this.left = left;
+                this.top = top;
+                this.right = right;
+                this.bottom = bottom;
+            }
+            public RECT(RECT rcSrc)
+            {
+                left = rcSrc.left;
+                top = rcSrc.top;
+                right = rcSrc.right;
+                bottom = rcSrc.bottom;
+            }
+            public bool IsEmpty { get { return left >= right || top >= bottom; } }
+            public override string ToString()
+            {
+                if (this == Empty) { return "RECT {Empty}"; }
+                return "RECT { left : " + left + " / top : " + top + " / right : " + right + " / bottom : " + bottom + " }";
+            }
+            public override bool Equals(object obj)
+            {
+                if (!(obj is System.Windows.Rect)) { return false; }
+                return (this == (RECT)obj);
+            }
+            /// <summary>Return the HashCode for this struct (not garanteed to be unique)</summary>
+            public override int GetHashCode() => left.GetHashCode() + top.GetHashCode() + right.GetHashCode() + bottom.GetHashCode();
+            /// <summary> Determine if 2 RECT are equal (deep compare)</summary>
+            public static bool operator ==(RECT rect1, RECT rect2) { return (rect1.left == rect2.left && rect1.top == rect2.top && rect1.right == rect2.right && rect1.bottom == rect2.bottom); }
+            /// <summary> Determine if 2 RECT are different(deep compare)</summary>
+            public static bool operator !=(RECT rect1, RECT rect2) { return !(rect1 == rect2); }
+        }
+
+
+        #endregion
     }
 }
