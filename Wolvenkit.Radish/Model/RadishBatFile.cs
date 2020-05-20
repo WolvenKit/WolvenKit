ecab5022c68529b7b3183e9192de4f667c388b19
rfuzzo <r.fuzzo@gmail.com>
2020-05-20 08:23:51 +0200

    solution cleanup
    - project naming cleanup
    - remove outdated radish tools

---
+++ Wolvenkit.Radish/Model/RadishBatFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WolvenKit.Radish.Model
+{
+    public class BatFile
+    {
+        public string Path { get; set; }
+        public string Name { get => System.IO.Path.GetFileName(Path); }
+        public bool Interactive { get; set; }
+
+        public BatFile(string path)
+        {
+            Path = path;
+        }
+    }
+}
---
+++ Wolvenkit.Radish/Model/RadishConfig.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WolvenKit.Radish.Model
+{
+    public class RadishConfig
+    {
+        public RadishConfig()
+        {
+
+        }
+        public static string ConfigurationPath
+        {
+            get
+            {
+                var path = Application.ExecutablePath;
+                var filename = Path.GetFileNameWithoutExtension(path);
+                var dir = Path.GetDirectoryName(path);
+                return Path.Combine(dir ?? "", filename + "_radishconfig.xml");
+            }
+        }
+        public List<RadishWorkflow> Workflows { get; set; } = new List<RadishWorkflow>();
+
+        public void Save()
+        {
+            // serialize workflows
+            var ser = new XmlSerializer(typeof(RadishConfig));
+            var stream = new FileStream(RadishConfig.ConfigurationPath, FileMode.Create, FileAccess.Write);
+            ser.Serialize(stream, this);
+            stream.Close();
+        }
+
+        public static RadishConfig Load()
+        {
+            if (File.Exists(ConfigurationPath) && new FileInfo(ConfigurationPath).Length != 0)
+            {
+                var ser = new XmlSerializer(typeof(RadishConfig));
+                var stream = new FileStream(ConfigurationPath, FileMode.Open, FileAccess.Read);
+                var config = (RadishConfig)ser.Deserialize(stream);
+                stream.Close();
+                return config;
+            }
+
+            // Defaults
+            return new RadishConfig
+            {
+
+            };
+        }
+    }
+}
