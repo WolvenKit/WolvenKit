af0aff400540ad81ef0d587f9fbf183570a6f200
rfuzzo <r.fuzzo@gmail.com>
2020-09-03 21:30:54 +0200

    Update IWitcherArchive.cs

--- WolvenKit.Common/Model/IWitcherArchive.cs
+++ WolvenKit.Common/Model/IWitcherArchive.cs
@@ -7,9 +7,18 @@ using WolvenKit.Common.Model;
 
 namespace WolvenKit.Common
 {
+    /// <summary>
+    /// Top-level game file, holds archived game assets. Types :
+    /// .bundle, .cache (collision, texture, sound) .w3speech
+    /// </summary>
     public interface IWitcherArchive
     {
+        WitcherTreeNode RootNode { get; set; }
+        List<IWitcherFile> FileList { get; set; }
         EBundleType TypeName { get; }
+        List<string> Extensions { get; set; }
+        AutoCompleteStringCollection AutocompleteSource { get; set; }
+        Dictionary<string, List<IWitcherFile>> Items { get; set; }
         string FileName { get; set; }
     }
 }
