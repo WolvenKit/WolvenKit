36c1195ed96e0860c98dbe5ee5d05e08dff1048b
rfuzzo <r.fuzzo@gmail.com>
2020-10-30 10:59:53 +0100

    fix merge error

--- WolvenKit/Properties/Resources.Designer.cs
+++ WolvenKit/Properties/Resources.Designer.cs
@@ -423,16 +423,6 @@ namespace WolvenKit.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap git {
-            get {
-                object obj = ResourceManager.GetObject("git", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap home {
             get {
                 object obj = ResourceManager.GetObject("home", resourceCulture);
