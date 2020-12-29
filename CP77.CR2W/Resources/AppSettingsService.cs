4e6d386785f40e55562fb13d879c68f8653745b5
rfuzzo <r.fuzzo@gmail.com>
2020-12-29 14:40:03 +0100

    Update AppSettingsService.cs
    fix path

--- CP77.CR2W/Resources/AppSettingsService.cs
+++ CP77.CR2W/Resources/AppSettingsService.cs
@@ -10,7 +10,7 @@ namespace CP77.CR2W.Resources
 
         public string ETagPath => Path.Combine(ResourcesPath, "archivehashes-etag.txt");
         public string ArchiveHashesPath => Path.Combine(ResourcesPath, "archivehashes.txt");
-        public string LooseHashesPath => Path.Combine(ResourcesPath, "archivehashes.csv");
+        public string LooseHashesPath => Path.Combine(ResourcesPath, "loosehashes.txt");
         public string ArchiveHashesZipPath => Path.Combine(ResourcesPath, "archivehashes.zip");
     }
 }
