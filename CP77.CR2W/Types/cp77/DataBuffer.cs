30d4b85a6f0f129a5f7767fd609273c34c1f34b3
rfuzzo <r.fuzzo@gmail.com>
2021-01-02 20:43:52 +0100

    add dumped classes

---
+++ CP77.CR2W/Types/cp77/DataBuffer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+using FastMember;
+using WolvenKit.CR2W.Reflection;
+
+namespace WolvenKit.CR2W.Types
+{
+    [REDMeta]
+    public class DataBuffer : CVariable
+    {
+        
+        public DataBuffer(CR2WFile cr2w, CVariable parent, string name) : base(cr2w, parent, name) { }
+
+        public CBytes Buffer { get; set; }
+
+        public override void Read(BinaryReader file, uint size)
+        {
+            Buffer = new CBytes(cr2w, this, nameof(Buffer))
+            {
+                Bytes = new byte[0],
+                IsSerialized = true
+            };
+
+            Buffer.Read(file, size);
+        }
+    }
+
+    [REDMeta]
+    public class serializationDeferredDataBuffer : CVariable
+    {
+        
+        public serializationDeferredDataBuffer(CR2WFile cr2w, CVariable parent, string name) : base(cr2w, parent, name) { }
+
+        public CBytes Buffer { get; set; }
+
+        public override void Read(BinaryReader file, uint size)
+        {
+            Buffer = new CBytes(cr2w, this, nameof(Buffer))
+            {
+                Bytes = new byte[0],
+                IsSerialized = true
+            };
+
+            Buffer.Read(file, size);
+        }
+    }
+
+
+
+
+
+}
