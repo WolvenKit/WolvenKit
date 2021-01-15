93900baa10c9c4b2637140474d32d5bc9cca3086
traderain <hambalko.bence@gmail.com>
2021-01-15 18:49:09 +0100

    Revert "Getting on track with new branches locally."
    This reverts commit 48ecc40c8e6932851286e8cbb211afc39ce43ae4, reversing
    changes made to 452c9b6a34f18ba4e57c6994d1ddd97221fd8e38.
    
    # Conflicts:
    #	WolvenKit/App.xaml
    #	WolvenKit/Views/HomePageView.xaml.cs
    #	WolvenKit/Views/LogView.xaml.cs

--- WolvenKit/Views/HomePageView.xaml.cs
+++ WolvenKit/Views/HomePageView.xaml.cs
@@ -10,6 +10,8 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using MahApps.Metro.Controls;
 
 namespace WolvenKit.Views
 {
@@ -20,19 +22,30 @@ namespace WolvenKit.Views
             InitializeComponent();
 
 
+   
             InitiializeGitHub();
         }
 
         private async void InitiializeGitHub()
         {
             GhubClient = new GitHubClient(new ProductHeaderValue("WolvenKit"));
-            GhubClient.Credentials = GhubAuth("wolvenbot", "botwolven1");            
-            await GhubLastReleaseAsync();
+            GhubClient.Credentials = GhubAuth("wolvenbot", "botwolven1");     
+            
+
+            await GhubLastReleaseAsync(); 
+            
+
+
+
+
         }
 
         GitHubClient GhubClient;
 
 
+
+
+        
 
         public Credentials GhubAuth(string u, string p)
         {
@@ -44,54 +57,76 @@ namespace WolvenKit.Views
 
         public async Task GhubLastReleaseAsync()
         {
-            var releases = await GhubClient.Repository.Release.GetAll("WolvenKit", "Wolven-Kit");
-            var latest = releases[0];
-
-
-            ObservableCollection<GithubTimeLine> data = new ObservableCollection<GithubTimeLine>();
-
-            var item = new GithubTimeLine() { TitleLabel = latest.TagName, TitleInfo = latest.Name, TitleStyle = ResourceHelper.GetResource<Style>(ResourceToken.LabelViolet) };
-
-            var unresolvedbody = latest.Body;
-            var body = await ResolveBody(unresolvedbody);
-
-            foreach (var line in body)
+            
+            try
             {
-                if (!line.Contains('#'))
+                var general = await GhubClient.Repository.Get("WolvenKit", "Wolven-Kit");
+                var g_stars = general.StargazersCount;
+                var g_forks = general.ForksCount;
+                var g_watchers = general.SubscribersCount;
+
+
+                WatchShield.SetCurrentValue(Shield.StatusProperty, g_watchers.ToString());
+                ForkShield.SetCurrentValue(Shield.StatusProperty, g_forks.ToString());
+                StarShield.SetCurrentValue(Shield.StatusProperty, g_stars.ToString());
+
+
+
+
+
+
+                var releases = await GhubClient.Repository.Release.GetLatest("WolvenKit", "Wolven-Kit");
+                var latest = releases; // Just a temp fix so I don't spam GHub api during dev
+                ObservableCollection<GithubTimeLine> data = new ObservableCollection<GithubTimeLine>();
+
+                var item = new GithubTimeLine() { TitleLabel = latest.TagName, TitleInfo = latest.Name, TitleStyle = ResourceHelper.GetResource<Style>(ResourceToken.LabelViolet) };
+
+                var unresolvedbody = latest.Body;
+                var body = await ResolveBody(unresolvedbody);
+
+                foreach (var line in body)
                 {
-                    string[] myStrings = { "more", "extra", "improved" };
-
-                    if (myStrings.Any(line.ToLowerInvariant().Contains))
+                    if (!line.Contains('#'))
                     {
-                        item.Members.Add(new ContentMember() { ContentTitle = "Addon", ContentInfo = line, ContentStyle = ResourceHelper.GetResource<Style>(ResourceToken.LabelInfo) });
+                        string[] myStrings = { "more", "extra", "improved" };
 
-                    }
-                    else if (line.ToLowerInvariant().Contains("new"))
-                    {
-                        item.Members.Add(new ContentMember() { ContentTitle = "New", ContentInfo = line, ContentStyle = ResourceHelper.GetResource<Style>(ResourceToken.LabelSuccess) });
+                        if (myStrings.Any(line.ToLowerInvariant().Contains))
+                        {
+                            item.Members.Add(new ContentMember() { ContentTitle = "Addon", ContentInfo = line, ContentStyle = ResourceHelper.GetResource<Style>(ResourceToken.LabelInfo) });
 
-                    }
-                    else if (line.ToLowerInvariant().Contains("breaking change"))
-                    {
-                        item.Members.Add(new ContentMember() { ContentTitle = "Breaking", ContentInfo = line, ContentStyle = ResourceHelper.GetResource<Style>(ResourceToken.LabelDanger) });
+                        }
+                        else if (line.ToLowerInvariant().Contains("new"))
+                        {
+                            item.Members.Add(new ContentMember() { ContentTitle = "New", ContentInfo = line, ContentStyle = ResourceHelper.GetResource<Style>(ResourceToken.LabelSuccess) });
 
-                    }
-                    else if (line.ToLowerInvariant().Contains("overhaul"))
-                    {
-                        item.Members.Add(new ContentMember() { ContentTitle = "Overhaul", ContentInfo = line, ContentStyle = ResourceHelper.GetResource<Style>(ResourceToken.LabelWarning) });
+                        }
+                        else if (line.ToLowerInvariant().Contains("breaking change"))
+                        {
+                            item.Members.Add(new ContentMember() { ContentTitle = "Breaking", ContentInfo = line, ContentStyle = ResourceHelper.GetResource<Style>(ResourceToken.LabelDanger) });
 
-                    }
-                    else
-                    {
-                        item.Members.Add(new ContentMember() { ContentTitle = "Change", ContentInfo = line, ContentStyle = ResourceHelper.GetResource<Style>(ResourceToken.LabelPrimary) });
+                        }
+                        else if (line.ToLowerInvariant().Contains("overhaul"))
+                        {
+                            item.Members.Add(new ContentMember() { ContentTitle = "Overhaul", ContentInfo = line, ContentStyle = ResourceHelper.GetResource<Style>(ResourceToken.LabelWarning) });
+
+                        }
+                        else
+                        {
+                            item.Members.Add(new ContentMember() { ContentTitle = "Change", ContentInfo = line, ContentStyle = ResourceHelper.GetResource<Style>(ResourceToken.LabelPrimary) });
+
+                        }
 
                     }
 
                 }
-
+                data.Add(item);
+                gitTime.SetCurrentValue(ItemsControl.ItemsSourceProperty, data);
             }
-            data.Add(item);
-            gitTime.SetCurrentValue(ItemsControl.ItemsSourceProperty, data);
+            catch { }
+
+
+
+           
 
         }
 
