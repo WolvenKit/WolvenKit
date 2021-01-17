3b797af9509c8e9fe6eb13532635985d2a4e7fa8
R503B <sodanak1n@outlook.com>
2021-01-17 02:27:00 +0100

    Design : First Setup Wizard Details : Started base design of pages.

---
+++ WolvenKit/ViewModels/Wizards/WizardPages/FirstSetupWizard/FinalizeSetupViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Catel.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WolvenKit.ViewModels.Wizards.WizardPages.FirstSetupWizard
+{
+    class FinalizeSetupViewModel : ViewModelBase
+    {
+    }
+}
