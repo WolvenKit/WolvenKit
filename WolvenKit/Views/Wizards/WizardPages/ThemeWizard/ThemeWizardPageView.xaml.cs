dc15024d2fbc12aa742fa3f3b97d4342f4a69bea
R503B <sodanak1n@outlook.com>
2021-01-19 14:18:59 +0100

    Basic Design : ThemeWizard Design : ThemeWizard Details : Made a basic themepicker/creator will improve on the looks later.

---
+++ WolvenKit/Views/Wizards/WizardPages/ThemeWizard/ThemeWizardPageView.xaml.cs
@@ -0,0 +1,101 @@
+﻿
+using ControlzEx.Theming;
+using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WolvenKit.Views.Wizards.WizardPages.ThemeWizard
+{
+    public partial class ThemeWizardPageView
+    {
+        public ThemeWizardPageView()
+        {
+            InitializeComponent();
+        }
+        private bool filled = false;
+
+
+        private void Circle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var a = (Ellipse)sender;
+
+            ControlzEx.Theming.ThemeManager.Current.ChangeTheme(Application.Current, "Dark." + a.Name.ToString());
+
+
+        }
+
+        private void Circle_MouseLeave(object sender, MouseEventArgs e)
+        {
+            var a = (Ellipse)sender;
+            a.Fill = lastaccent;
+        }
+        private Brush lastaccent;
+        private void Circle_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var a = (Ellipse)sender;
+            lastaccent = a.Fill;
+            a.Fill = new SolidColorBrush(Colors.AliceBlue);
+        }
+
+
+
+
+
+        private void ColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+
+        }
+
+        private void UserControl_Initialized_1(object sender, EventArgs e)
+        {
+            if (!filled)
+            {
+
+                (new Thread(() =>
+                {
+
+
+
+                    foreach (var Theme in ThemeManager.Current.Themes)
+                    {
+                        Thread.Sleep(15);
+                        if (Theme.BaseColorScheme == "Dark")
+                        {
+                            if (!Theme.DisplayName.Contains("Colorful"))
+                            {
+                                Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+                                {
+
+                                    Ellipse circle = new Ellipse();
+                                    circle.Height = 25;
+                                    circle.Width = 25;
+                                    circle.Margin = new Thickness(5);
+                                    circle.MouseEnter += Circle_MouseEnter;
+                                    circle.MouseLeave += Circle_MouseLeave;
+                                    circle.MouseLeftButtonDown += Circle_MouseLeftButtonDown;
+                                    circle.Name = Theme.DisplayName.Split('(')[0].ToString().Trim();
+                                    circle.Fill = Theme.ShowcaseBrush;
+
+                                    HoneyColors.Children.Add(circle);
+
+                                }));
+                            }
+
+
+
+
+                        }
+                    }
+
+
+
+                })).Start();
+
+            }
+            filled = true;
+        }
+    }
+}
