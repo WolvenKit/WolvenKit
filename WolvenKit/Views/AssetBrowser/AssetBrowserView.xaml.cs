f41d914679e8a2cc59b131a153b299e4242af48f
R503B <sodanak1n@outlook.com>
2021-01-20 16:34:39 +0100

    Window Functionality : Asset Browser Details : Added Drag , Resize , Minimize and Close Window Functionality. ( NON MVVM)

--- WolvenKit/Views/AssetBrowser/AssetBrowserView.xaml.cs
+++ WolvenKit/Views/AssetBrowser/AssetBrowserView.xaml.cs
@@ -11,5 +11,23 @@ namespace WolvenKit.Views.AssetBrowser
             ControlzEx.Theming.ThemeManager.Current.ChangeTheme(this, "Dark.Red"); //This aint needed was just for testing remove me.
 
         }
+
+        private void DraggableTitleBar_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+
+            // Begin dragging the window
+            this.DragMove();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, System.Windows.RoutedEventArgs e)
+        {
+            SetCurrentValue(WindowStateProperty, System.Windows.WindowState.Minimized);
+        }
     }
 }
