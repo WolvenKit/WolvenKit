ce188ee8576be62bf68309b7ab14d9ed9414a246
R503B <sodanak1n@outlook.com>
2021-01-23 00:16:39 +0100

    Plugin Manager is MVVM convert ready.

--- WolvenKit/Views/AssetBrowser/AssetBrowserView.xaml.cs
+++ WolvenKit/Views/AssetBrowser/AssetBrowserView.xaml.cs
@@ -8,7 +8,6 @@ namespace WolvenKit.Views.AssetBrowser
         public AssetBrowserView() : base(DataWindowMode.Custom)
         {
             InitializeComponent();
-            ControlzEx.Theming.ThemeManager.Current.ChangeTheme(this, "Dark.Red"); //This aint needed was just for testing remove me.
 
         }
 
