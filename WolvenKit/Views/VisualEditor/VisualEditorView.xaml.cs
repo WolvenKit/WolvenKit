988b7eb2820847fea9ec40c9b72486275ef828ff
R503B <sodanak1n@outlook.com>
2021-01-24 11:35:43 +0100

    ok

---
+++ WolvenKit/Views/VisualEditor/VisualEditorView.xaml.cs
@@ -0,0 +1,26 @@
+﻿
+using Catel.Windows;
+using DynamicData;
+using NodeNetwork.ViewModels;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WolvenKit.Views.VisualEditor
+{
+    public partial class VisualEditorView
+    {
+
+
+        public VisualEditorView() : base(DataWindowMode.Custom)
+        {
+            InitializeComponent(); 
+            
+
+         
+
+        }
+    }
+}
