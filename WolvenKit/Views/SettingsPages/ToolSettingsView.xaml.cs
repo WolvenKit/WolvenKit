e6f05d56e9682c669f1191dfe68d06bd22f1b152
R503B <sodanak1n@outlook.com>
2021-01-29 16:36:03 +0100

    Persistance for GeneralSettingsView. Toolsettings View Design.

--- WolvenKit/Views/SettingsPages/ToolSettingsView.xaml.cs
+++ WolvenKit/Views/SettingsPages/ToolSettingsView.xaml.cs
@@ -1,4 +1,6 @@
 ﻿
+using WolvenKit.Views.SettingsPages.SubPages.Tool;
+
 namespace WolvenKit.Views.SettingsPages
 {
     public partial class ToolSettingsView
@@ -6,6 +8,42 @@ namespace WolvenKit.Views.SettingsPages
         public ToolSettingsView()
         {
             InitializeComponent();
+        }
+
+        private void AssetBrowserSubItem_Selected(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (IsLoaded && IsVisible && IsInitialized)
+            {
+                SettingsViewer.Children.Clear();
+                SettingsViewer.Children.Add(new AssetBrowserSubSettingsView());
+            }
+        }
+
+        private void PluginManagerSubItem_Selected(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (IsLoaded && IsVisible && IsInitialized)
+            {
+                SettingsViewer.Children.Clear();
+                SettingsViewer.Children.Add(new PluginManagerSubSettingsView());
+            }
+        }
+
+        private void CodeEditorSubItem_Selected(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (IsLoaded && IsVisible && IsInitialized)
+            {
+                SettingsViewer.Children.Clear();
+                SettingsViewer.Children.Add(new CodeEditorSubSettingsView());
+            }
+        }
+
+        private void VisualEditorSubItem_Selected(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (IsLoaded && IsVisible && IsInitialized)
+            {
+                SettingsViewer.Children.Clear();
+                SettingsViewer.Children.Add(new VisualEditorSubSettingsView());
+            }
         }
     }
 }
