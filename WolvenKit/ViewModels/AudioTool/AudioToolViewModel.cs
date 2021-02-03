9de2a9596b44f0c98900a411ebb49b8ac22c7bf3
R503B <sodanak1n@outlook.com>
2021-02-03 11:59:41 +0100

    AudioToolDesign And MVVM headaches

--- WolvenKit/ViewModels/AudioTool/AudioToolViewModel.cs
+++ WolvenKit/ViewModels/AudioTool/AudioToolViewModel.cs
@@ -1,13 +1,44 @@
 ﻿using Catel.MVVM;
+using FmodAudio;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WolvenKit.Views.AudioTool;
 
 namespace WolvenKit.ViewModels.AudioTool
 {
     class AudioToolViewModel : ViewModelBase
     {
+
+
+        private Channel Channel_;
+
+        public AudioToolViewModel(Channel channel)
+        {
+            Channel_ = channel;
+            CurrentPlayerTime = TimeSpan.FromMilliseconds(channel.GetPosition(TimeUnit.MS));
+        }
+
+        public TimeSpan CurrentPlayerTime
+        {
+            get
+            {
+
+
+
+                return TimeSpan.FromMilliseconds(Channel_.GetPosition(TimeUnit.MS)); 
+            }
+            set
+            {
+                RaisePropertyChanged(nameof(CurrentPlayerTime));
+
+            }
+
+        }
+
+
+
     }
 }
--- WolvenKit/Views/AudioTool/AudioToolView.xaml.cs
+++ WolvenKit/Views/AudioTool/AudioToolView.xaml.cs
@@ -1,20 +1,54 @@
 ﻿
 using Catel.Windows;
 using FmodAudio;
+using System;
+using System.Windows;
+using WolvenKit.ViewModels.AudioTool;
 
 namespace WolvenKit.Views.AudioTool
 {
     public partial class AudioToolView
     {
+
+        public Channel channel;
         public AudioToolView() : base(DataWindowMode.Custom)
         {
             InitializeComponent();
             Systemz = Fmod.CreateSystem(); 
             Systemz.Init(2);
 
-            var sound = Systemz.CreateSound("testmp3.mp3", FmodAudio.Mode.CreateStream);
+            Sound sound = Systemz.CreateSound("testmp3.mp3", FmodAudio.Mode.CreateStream);
 
-          var  channel = Systemz.PlaySound(sound);
+          
+
+            channel = Systemz.PlaySound(sound);
+
+
+            TimeSpan.FromSeconds(sound.GetLength(TimeUnit.MS) * 1000);
+
+
+            ResourceDictionary themeResources = Application.LoadComponent(new Uri("Resources/Styles/ExpressionDark.xaml", UriKind.Relative)) as ResourceDictionary;
+            Resources.MergedDictionaries.Add(themeResources);
+            //clockDisplay.SetCurrentValue(WPFSoundVisualizationLib.DigitalClock.TimeProperty, TimeSpan.FromMilliseconds(channel.CurrentSound.GetLength(TimeUnit.MS)));
+
+            this.DataContext = new AudioToolViewModel(channel);
+
+
+
+        }
+
+
+        public delegate void MyPersonalizedUCEventHandler(bool sampleParam);
+
+        public event MyPersonalizedUCEventHandler MyEvent;
+
+        public void RaiseMyEvent()
+        {
+            // Your logic
+            if (MyEvent != null)
+            {
+                MyEvent(false);
+            }
         }
 
         protected FmodSystem Systemz;
@@ -37,6 +71,23 @@ namespace WolvenKit.Views.AudioTool
         {
             SetCurrentValue(WindowStateProperty, System.Windows.WindowState.Minimized);
         }
+
+
+
+
+
+
+        private void SetPlayerTimeToDigitalClock()
+        {
+            clockDisplay.SetCurrentValue(WPFSoundVisualizationLib.DigitalClock.TimeProperty, TimeSpan.FromMilliseconds(channel.GetPosition(TimeUnit.MS)));
+
+        }
+        private void SetTrackLengthToDigitalClock()
+        {
+
+        }
+
+
     }
 
 }
