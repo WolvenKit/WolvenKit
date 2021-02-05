7988e3803387820fcde232d7338eedc6401c7fb8
rfuzzo <r.fuzzo@gmail.com>
2021-02-05 21:20:24 +0100

    order props

--- CP77.CR2W/Types/cp77/EffectExecutor_SendActionSignal.cs
+++ CP77.CR2W/Types/cp77/EffectExecutor_SendActionSignal.cs
@@ -8,8 +8,8 @@ namespace CP77.CR2W.Types
 	[REDMeta]
 	public class EffectExecutor_SendActionSignal : gameEffectExecutor_Scripted
 	{
-		[Ordinal(0)]  [RED("signalDuration")] public CFloat SignalDuration { get; set; }
-		[Ordinal(1)]  [RED("signalName")] public CName SignalName { get; set; }
+        [Ordinal(0)] [RED("signalName")] public CName SignalName { get; set; }
+        [Ordinal(1)] [RED("signalDuration")] public CFloat SignalDuration { get; set; }
 
 		public EffectExecutor_SendActionSignal(CR2WFile cr2w, CVariable parent, string name) : base(cr2w, parent, name) { }
 	}
--- CP77.CR2W/Types/cp77/HitShapeUserDataBase.cs
+++ CP77.CR2W/Types/cp77/HitShapeUserDataBase.cs
@@ -8,13 +8,13 @@ namespace CP77.CR2W.Types
 	[REDMeta]
 	public class HitShapeUserDataBase : gameHitShapeUserData
 	{
-		[Ordinal(0)]  [RED("dismembermentPart")] public CEnum<EAIDismembermentBodyPart> DismembermentPart { get; set; }
-		[Ordinal(1)]  [RED("hitReactionZone")] public CEnum<EHitReactionZone> HitReactionZone { get; set; }
-		[Ordinal(2)]  [RED("hitShapeDamageMod")] public CFloat HitShapeDamageMod { get; set; }
-		[Ordinal(3)]  [RED("hitShapeTag")] public CName HitShapeTag { get; set; }
-		[Ordinal(4)]  [RED("hitShapeType")] public CEnum<EHitShapeType> HitShapeType { get; set; }
-		[Ordinal(5)]  [RED("isInternalWeakspot")] public CBool IsInternalWeakspot { get; set; }
-		[Ordinal(6)]  [RED("isProtectionLayer")] public CBool IsProtectionLayer { get; set; }
+        [Ordinal(1)] [RED("hitShapeTag")] public CName HitShapeTag { get; set; }
+        [Ordinal(2)] [RED("hitShapeType")] public CEnum<EHitShapeType> HitShapeType { get; set; }
+        [Ordinal(3)] [RED("hitReactionZone")] public CEnum<EHitReactionZone> HitReactionZone { get; set; }
+        [Ordinal(4)] [RED("dismembermentPart")] public CEnum<EAIDismembermentBodyPart> DismembermentPart { get; set; }
+        [Ordinal(5)] [RED("isProtectionLayer")] public CBool IsProtectionLayer { get; set; }
+        [Ordinal(6)] [RED("isInternalWeakspot")] public CBool IsInternalWeakspot { get; set; }
+        [Ordinal(7)] [RED("hitShapeDamageMod")] public CFloat HitShapeDamageMod { get; set; }
 
 		public HitShapeUserDataBase(CR2WFile cr2w, CVariable parent, string name) : base(cr2w, parent, name) { }
 	}
