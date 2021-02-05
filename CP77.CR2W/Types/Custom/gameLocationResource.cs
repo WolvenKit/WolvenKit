fb921e739af0abfe73898cb474fac147e56359c1
rfuzzo <r.fuzzo@gmail.com>
2021-02-05 11:51:14 +0100

    loot, location unknown bytes

---
+++ CP77.CR2W/Types/Custom/gameLocationResource.cs
@@ -0,0 +1,48 @@
+using System;
+using System.IO;
+using Catel.IO;
+using CP77.CR2W.Reflection;
+using FastMember;
+using static CP77.CR2W.Types.Enums;
+
+namespace CP77.CR2W.Types
+{
+	[REDMeta]
+	public class gameLocationResource : CResource
+	{
+        private const uint Version = 0;
+
+		public gameLocationResource(CR2WFile cr2w, CVariable parent, string name) : base(cr2w, parent, name) { }
+
+        public override void Read(BinaryReader file, uint size)
+        {
+            base.Read(file, size);
+
+            var version = file.ReadUInt32();
+            if (version != Version)
+                throw new NotImplementedException($"{nameof(gameLocationResource)} - version mismatch");
+            var datasize = file.ReadUInt32() - 4;
+
+            var count = file.ReadVLQInt32();
+            for (int i = 0; i < count; i++)
+            {
+                // unused
+            }
+
+        }
+
+        public override void Write(BinaryWriter file)
+        {
+            base.Write(file);
+
+            file.Write(Version);
+
+            using var ms = new MemoryStream();
+            using var bw = new BinaryWriter(ms);
+            bw.Write((byte)0x00); //TODO
+
+            file.Write((uint)ms.Length + 4);
+            file.Write(ms.ToByteArray());
+        }
+    }
+}
