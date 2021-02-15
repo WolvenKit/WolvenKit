0e60a50c7397bf489bab850829b6fa2c6a29322c
Octavian Dima <WopsS@users.noreply.github.com>
2021-02-15 22:08:03 +0200

    Remove unused namespace from generated types (#363)
    * Remove unused namespace from generated types
    
    * Remove more useless usings

--- CP77.CR2W/Types/cp77/animLookAtPartInfo_.cs
+++ CP77.CR2W/Types/cp77/animLookAtPartInfo_.cs
@@ -1,4 +1,3 @@
-using System.IO;
 using CP77.CR2W.Reflection;
 using FastMember;
 using static CP77.CR2W.Types.Enums;
--- CP77.CR2W/Types/cp77/gameMovingPlatformBeforeArrivedAt.cs
+++ CP77.CR2W/Types/cp77/gameMovingPlatformBeforeArrivedAt.cs
@@ -1,7 +1,4 @@
-using System.IO;
 using CP77.CR2W.Reflection;
-using FastMember;
-using static CP77.CR2W.Types.Enums;
 
 namespace CP77.CR2W.Types
 {
