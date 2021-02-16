75f11f1ad23372310e73855755e4d584ff1e7164
Offline <11785451+Offline-R503B@users.noreply.github.com>
2021-02-16 12:03:24 -0800

    CodeEditorWork + Fody Fix.  (#364)
    * CodeEditorWork + Fody Fix. (First build might still give the error after the 2nd time it should be fine , you could also clean + rebuild and even remove the older packages of fody in their nuget folder (locally)
    
    * TextEditLib Net5
    
    * Text Edit Lib Based on EDI Update.
    
    * Fix build
    
    * Edi Extras Into TextEditLib
    
    * debug page added

---
+++ TextEditLib/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace TexteditLib.ViewModels.Base
+{
+	/// <summary>
+	/// Implements a base class for all viewmodel classes
+	/// that implements <seealso cref="INotifyPropertyChanged"/> interface for binding.
+	/// </summary>
+	public class ViewModelBase : INotifyPropertyChanged
+	{
+		/// <summary>
+		/// Standard implementation of <seealso cref="INotifyPropertyChanged"/>.
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		/// <summary>
+		/// Tell bound controls (via WPF binding) to refresh their display.
+		/// 
+		/// Sample call: this.NotifyPropertyChanged(() => this.IsSelected);
+		/// where 'this' is derived from <seealso cref="ViewModelBase"/>
+		/// and IsSelected is a property.
+		/// </summary>
+		/// <typeparam name="TProperty"></typeparam>
+		/// <param name="property"></param>
+		public void NotifyPropertyChanged<TProperty>(Expression<Func<TProperty>> property)
+		{
+			var lambda = (LambdaExpression)property;
+			MemberExpression memberExpression;
+
+			if (lambda.Body is UnaryExpression)
+			{
+				var unaryExpression = (UnaryExpression)lambda.Body;
+				memberExpression = (MemberExpression)unaryExpression.Operand;
+			}
+			else
+				memberExpression = (MemberExpression)lambda.Body;
+
+			this.RaisePropertyChanged(memberExpression.Member.Name);
+		}
+
+		/// <summary>
+		/// Tell bound controls (via WPF binding) to refresh their display.
+		/// Standard implementation through <seealso cref="INotifyPropertyChanged"/>.
+		/// </summary>
+		/// <param name="propertyName"></param>
+		protected virtual void RaisePropertyChanged(string propertyName)
+		{
+			if (this.PropertyChanged != null)
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
