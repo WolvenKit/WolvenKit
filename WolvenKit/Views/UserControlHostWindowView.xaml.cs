19563a65530b7221363c598c42c71c59ff46b6d6
Ákos Köte <kote2ster@gmail.com>
2021-02-18 01:33:52 +0100

    Update project wizard vms

--- WolvenKit/Views/UserControlHostWindowView.xaml.cs
+++ WolvenKit/Views/UserControlHostWindowView.xaml.cs
@@ -13,7 +13,10 @@ namespace WolvenKit.Views
             InitializeComponent();
 
             UserContentControl.Content = ucvm.ContentUserControl;
-            
+            ucvm.ClosedAsync += (s, e) =>
+            {
+                return System.Threading.Tasks.Task.Run(() => Dispatcher.Invoke(() => Close()));
+            };
         }
 
         private void DraggableTitleBar_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
--- WolvenKit/Views/Wizards/WizardPages/ProjectWizard/FinalizeSetupView.xaml.cs
+++ WolvenKit/Views/Wizards/WizardPages/ProjectWizard/FinalizeSetupView.xaml.cs
@@ -1,25 +1,62 @@
 ﻿
 using Catel.IoC;
+using HandyControl.Controls;
 using WolvenKit.ViewModels.Wizards;
 
 namespace WolvenKit.Views.Wizards.WizardPages.ProjectWizard
 {
     public partial class FinalizeSetupView
     {
-        ProjectWizardView _pwv;
+        ProjectWizardViewModel _pwvm;
 
-        public FinalizeSetupView(ProjectWizardView pwv)
+        public FinalizeSetupView()
         {
             InitializeComponent();
 
-            _pwv = pwv;
             var command = ServiceLocator.Default.ResolveType<ApplicationCreateNewProjectCommandContainer>();
             command.OnCommandCompleted += () => CancelProjectBtn_Click(null, null);
+
+            _pwvm = ServiceLocator.Default.ResolveType<ProjectWizardViewModel>();
+            imgSelector.CommandBindings[0].Executed += imgSelector_Executed;
         }
+
+        private void UserControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (_pwvm.ProfileImageBrush != null)
+            {
+                imgSelector.SetValue(ImageSelector.UriPropertyKey, new System.Uri(_pwvm.ProfileImageBrushPath, System.UriKind.RelativeOrAbsolute));
+                imgSelector.SetValue(ImageSelector.PreviewBrushPropertyKey, _pwvm.ProfileImageBrush);
+                imgSelector.SetValue(ImageSelector.HasValuePropertyKey, true);
+                imgSelector.SetCurrentValue(ImageSelector.ToolTipProperty, _pwvm.ProfileImageBrushPath);
+            }
+            else
+            {
+                imgSelector.SetValue(ImageSelector.UriPropertyKey, default(System.Uri));
+                imgSelector.SetValue(ImageSelector.PreviewBrushPropertyKey, default(System.Windows.Media.Brush));
+                imgSelector.SetValue(ImageSelector.HasValuePropertyKey, false);
+                imgSelector.SetCurrentValue(ImageSelector.ToolTipProperty, default);
+            }
+        }
+
+        private void imgSelector_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            var imgBrush = imgSelector.PreviewBrush as System.Windows.Media.ImageBrush;
+            if (imgBrush != null)
+            {
+                _pwvm.ProfileImageBrush = imgBrush;
+                _pwvm.ProfileImageBrushPath = imgSelector.Uri.AbsoluteUri;
+            }
+            else
+            {
+                _pwvm.ProfileImageBrush = default(System.Windows.Media.ImageBrush);
+                _pwvm.ProfileImageBrushPath = "";
+            }
+        }
+
 
         private void CancelProjectBtn_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            (_pwv.ViewModel as ProjectWizardViewModel)?.Close();
+            _pwvm.CloseViewModelAsync(null);
         }
     }
 }
