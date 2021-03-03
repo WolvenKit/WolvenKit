22d676b7a50cd5b7a0ddafcc01f08990e60aa3ed
R503B <sodanak1n@outlook.com>
2021-03-03 17:22:03 +0100

    CodeMaid On Nodes

--- WolvenKit/MVVM/Views/Components/Editors/VisualEditor/Nodes/Operations/IN_Node.xaml.cs
+++ WolvenKit/MVVM/Views/Components/Editors/VisualEditor/Nodes/Operations/IN_Node.xaml.cs
@@ -1,19 +1,6 @@
-using ReactiveUI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reactive.Disposables;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ReactiveUI;
 
 namespace WolvenKit.MVVM.Views.Components.Editors.VisualEditor.Nodes
 {
