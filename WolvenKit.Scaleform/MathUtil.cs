00695e6389f48e3cd9b15f2d3e9b7aa4d99d7b18
R503B <sodanak1n@outlook.com>
2021-03-04 00:52:53 +0100

    ScaleForm

--- WolvenKit.Scaleform/MathUtil.cs
+++ WolvenKit.Scaleform/MathUtil.cs
@@ -1,53 +1,19 @@
-﻿using Microsoft.CSharp;
 using System;
 using System.CodeDom.Compiler;
 using System.Reflection;
 using System.Text;
+using Microsoft.CSharp;
+
 //using System.Windows.Forms;
 
 namespace WolvenKit.Scaleform
 {
     public class MathUtil
     {
-        public static string Evaluate(string expression)
-        {
-            object results = null;
-            string outputValue;
-
-            try
-            {
-                results = Eval(expression);
-                outputValue = System.Convert.ToString((long)System.Convert.ToUInt64(results), 10);
-            }
-            catch (Exception)
-            {
-                outputValue = null;
-            }
-
-            return outputValue;
-        }
-
-        public static long RoundUpToByteAlignment(long valueToRound, long byteAlignment)
-        {
-            long roundedValue = -1;
-
-            roundedValue = (valueToRound + byteAlignment - 1)/byteAlignment*byteAlignment;
-
-            return roundedValue;
-        }
-
-        public static ulong RoundUpToByteAlignment(ulong valueToRound, ulong byteAlignment)
-        {
-            ulong roundedValue;
-
-            roundedValue = (valueToRound + byteAlignment - 1)/byteAlignment*byteAlignment;
-
-            return roundedValue;
-        }
+        #region Methods
 
         public static object Eval(string sCSCode)
         {
-
             CSharpCodeProvider c = new CSharpCodeProvider();
             CompilerParameters cp = new CompilerParameters();
 
@@ -75,7 +41,7 @@ namespace WolvenKit.Scaleform
             sb.Append("} \n");
             sb.Append("} \n");
             sb.Append("}\n");
-            
+
             CompilerResults cr = c.CompileAssemblyFromSource(cp, sb.ToString());
             if (cr.Errors.Count > 0)
             {
@@ -93,7 +59,44 @@ namespace WolvenKit.Scaleform
 
             object s = mi.Invoke(o, null);
             return s;
-
         }
+
+        public static string Evaluate(string expression)
+        {
+            object results = null;
+            string outputValue;
+
+            try
+            {
+                results = Eval(expression);
+                outputValue = System.Convert.ToString((long)System.Convert.ToUInt64(results), 10);
+            }
+            catch (Exception)
+            {
+                outputValue = null;
+            }
+
+            return outputValue;
+        }
+
+        public static long RoundUpToByteAlignment(long valueToRound, long byteAlignment)
+        {
+            long roundedValue = -1;
+
+            roundedValue = (valueToRound + byteAlignment - 1) / byteAlignment * byteAlignment;
+
+            return roundedValue;
+        }
+
+        public static ulong RoundUpToByteAlignment(ulong valueToRound, ulong byteAlignment)
+        {
+            ulong roundedValue;
+
+            roundedValue = (valueToRound + byteAlignment - 1) / byteAlignment * byteAlignment;
+
+            return roundedValue;
+        }
+
+        #endregion Methods
     }
 }
