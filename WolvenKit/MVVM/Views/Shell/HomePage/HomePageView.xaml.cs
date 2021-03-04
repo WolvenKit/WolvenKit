b6aaa4de31a2b493ca7ec55c3a92d8f76e1bca4c
R503B <sodanak1n@outlook.com>
2021-03-04 12:45:32 +0100

    Cleaned Codebehind HomepageView ( Leftovers are Drag functionality)

--- WolvenKit/MVVM/Views/Shell/HomePage/HomePageView.xaml.cs
+++ WolvenKit/MVVM/Views/Shell/HomePage/HomePageView.xaml.cs
@@ -1,5 +1,4 @@
 using System.Windows;
-using Fluent;
 using WolvenKit.Functionality.WKitGlobal.Helpers;
 
 namespace WolvenKit.MVVM.Views.Shell.HomePage
@@ -16,20 +15,11 @@ namespace WolvenKit.MVVM.Views.Shell.HomePage
             GlobalHomePage = this;
         }
 
-        private void Grid_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            base.OnMouseLeftButtonDown(e);
-
-            StaticReferences.GlobalShell.DragMove();
-        }
 
         private void Grid_MouseLeftButtonDown_1(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             base.OnMouseLeftButtonDown(e);
-
             StaticReferences.GlobalShell.DragMove();
-
-            // Begin dragging the window
         }
 
 
@@ -42,35 +32,19 @@ namespace WolvenKit.MVVM.Views.Shell.HomePage
                 {
                     if (StaticReferences.GlobalShell.WindowState == WindowState.Maximized)
                     {
-                        StaticReferences.GlobalShell.SetCurrentValue(System.Windows.Window.WindowStateProperty, WindowState.Normal);
+                        StaticReferences.GlobalShell.SetCurrentValue(Window.WindowStateProperty, WindowState.Normal);
                     }
                     else
                     {
-                        StaticReferences.GlobalShell.SetCurrentValue(System.Windows.Window.WindowStateProperty, WindowState.Maximized);
+                        StaticReferences.GlobalShell.SetCurrentValue(Window.WindowStateProperty, WindowState.Maximized);
                     }
                 }
             }
             else
             {
                 base.OnMouseLeftButtonDown(e);
-
                 StaticReferences.GlobalShell.DragMove();
             }
-        }
-
-        //private void SideMenu_DebugItem_Selected(object sender, RoutedEventArgs e)
-        //{
-        //    if (IsLoaded && IsVisible && IsInitialized)
-        //    {
-        //        PageViewGrid.Children.Clear();
-        //        PageViewGrid.Children.Add(DebugPV);
-        //    }
-        //}
-
-        private void Image_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            StaticReferences.RibbonViewInstance.startScreen.SetCurrentValue(StartScreen.ShownProperty, false);
-            StaticReferences.RibbonViewInstance.startScreen.SetCurrentValue(Fluent.Backstage.IsOpenProperty, false);
         }
     }
 }
