ad6a6f86dace3d150d1d94403b1cb6af99d2e90c
rfuzzo <r.fuzzo@gmail.com>
2021-03-05 12:43:20 +0100

    add a bool converter

--- WolvenKit/MVVM/Views/PropertyGridEditors/ExpandableObjectEditor.cs
+++ WolvenKit/MVVM/Views/PropertyGridEditors/ExpandableObjectEditor.cs
@@ -1,12 +1,44 @@
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Media;
 using HandyControl.Controls;
 using WolvenKit.Common.Model.Cr2w;
 using WolvenKit.Common.Services;
 
 namespace WolvenKit.MVVM.Views.PropertyGridEditors
 {
+    [ValueConversion(typeof(bool), typeof(SolidColorBrush))]
+    public class BoolToBrushConverter : IValueConverter
+    {
+        public object
+            Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            // Setting default values
+            SolidColorBrush color;
+            var colorIfTrue = Colors.DarkOliveGreen;
+            var colorIfFalse = Colors.Red;
+
+            // Creating Color Brush
+            if ((bool)value)
+            {
+                color = new SolidColorBrush(colorIfTrue);
+            }
+            else
+            {
+                color = new SolidColorBrush(colorIfFalse);
+            }
+            return color;
+        }
+
+        // needs to be double to be able to convert from the Numeric Converter
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+
+
     public class ExpandableObjectEditor : EditorBase<IEditableVariable>, IExpandableObjectEditor
     {
         private protected override DependencyProperty GetInnerDependencyProperty() => PropertyGrid.SelectedObjectProperty;
@@ -20,6 +52,27 @@ namespace WolvenKit.MVVM.Views.PropertyGridEditors
         {
             var (tree, pg) = CreateCustomInnerElement(propertyItem);
             CreateInnerBinding(pg);
+
+            BindingOperations.SetBinding(
+                tree,
+                Control.BackgroundProperty,
+                new Binding($"{nameof(Wrapper)}.IsSerialized")
+                {
+                    Source = this,
+                    Mode = BindingMode.TwoWay,
+                    UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged,
+                    Converter = new BoolToBrushConverter()
+                });
+            BindingOperations.SetBinding(
+                pg,
+                Control.BackgroundProperty,
+                new Binding($"{nameof(Wrapper)}.IsSerialized")
+                {
+                    Source = this,
+                    Mode = BindingMode.TwoWay,
+                    UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged,
+                    Converter = new BoolToBrushConverter()
+                });
 
             return tree;
         }
