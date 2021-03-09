a636b5fe3f1e34339dcb8922a119887d4e124e69
R503B <sodanak1n@outlook.com>
2021-03-10 00:46:53 +0100

    More of the great movement. Going Well so far :D

--- WolvenKit.App/ViewModels/Dialogs/StringsGUIImporterIDDialogViewModel.cs
+++ WolvenKit.App/ViewModels/Dialogs/StringsGUIImporterIDDialogViewModel.cs
@@ -2,7 +2,7 @@ using Catel.MVVM;
 
 namespace WolvenKit.MVVM.ViewModels.Components.Dialogs
 {
-    internal class ExtractAmbigiousDialogViewModel : ViewModelBase
+    public class StringsGUIImporterIDDialogViewModel : ViewModelBase
     {
     }
 }
