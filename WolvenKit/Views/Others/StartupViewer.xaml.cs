74c09a1675470222f18f2eadbf361e1c0bace449
R503B <sodanak1n@outlook.com>
2021-04-03 01:41:12 +0200

    WIP

---
+++ WolvenKit/Views/Others/StartupViewer.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace WolvenKit.Views.Others
+{
+    /// <summary>
+    /// Interaction logic for StartupViewer.xaml
+    /// </summary>
+    public partial class StartupViewer : UserControl
+    {
+        public StartupViewer()
+        {
+            InitializeComponent();
+        }
+    }
+}
