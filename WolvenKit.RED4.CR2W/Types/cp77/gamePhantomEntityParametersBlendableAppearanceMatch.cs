3d86f45480493d66ed9a1fb7519231c9135e9466
seberoth <seberoth@users.noreply.github.com>
2021-04-14 16:44:17 +0200

    ...

---
+++ WolvenKit.RED4.CR2W/Types/cp77/gamePhantomEntityParametersBlendableAppearanceMatch.cs
@@ -0,0 +1,31 @@
+using WolvenKit.RED4.CR2W.Reflection;
+using FastMember;
+using static WolvenKit.RED4.CR2W.Types.Enums;
+
+namespace WolvenKit.RED4.CR2W.Types
+{
+	[REDMeta]
+	public class gamePhantomEntityParametersBlendableAppearanceMatch : CVariable
+	{
+		private CName _blendable;
+		private CName _notBlendable;
+
+		[Ordinal(0)] 
+		[RED("blendable")] 
+		public CName Blendable
+		{
+			get => GetProperty(ref _blendable);
+			set => SetProperty(ref _blendable, value);
+		}
+
+		[Ordinal(1)] 
+		[RED("notBlendable")] 
+		public CName NotBlendable
+		{
+			get => GetProperty(ref _notBlendable);
+			set => SetProperty(ref _notBlendable, value);
+		}
+
+		public gamePhantomEntityParametersBlendableAppearanceMatch(CR2WFile cr2w, CVariable parent, string name) : base(cr2w, parent, name) { }
+	}
+}
