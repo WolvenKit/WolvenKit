2b14c328d60577f8873681674640fe90d039448e
R503B <sodanak1n@outlook.com>
2021-04-24 13:51:37 +0200

    Very important update

--- WolvenKit/Views/Shell/StatusBarView.xaml.cs
+++ WolvenKit/Views/Shell/StatusBarView.xaml.cs
@@ -1,3 +1,8 @@
+using System;
+using Catel.IoC;
+using CatFacts.Net;
+using Orchestra.Services;
+
 namespace WolvenKit.Views.Shell
 {
     public partial class StatusBarView
@@ -10,5 +15,23 @@ namespace WolvenKit.Views.Shell
         }
 
         #endregion Constructors
+
+        private async void Tag_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var client = new CatFactsClient();
+            var randomFact = await client.GetRandomFactsAsync();
+            try
+            {
+                Random x = new Random();
+                var z = x.Next(0, randomFact.Length);
+                ServiceLocator.Default.ResolveType<IGrowlNotificationService>().Info(randomFact[z].Text);
+
+            }
+            catch
+            {
+
+            }
+
+        }
     }
 }
