64c5f9eba152b85f9436bccd09bb3ebc6766dfac
R503B <sodanak1n@outlook.com>
2021-05-27 09:37:02 +0200

    VM cleanup

--- WolvenKit.App/ViewModels/Editor/Basic/PropertiesViewModel.cs
+++ WolvenKit.App/ViewModels/Editor/Basic/PropertiesViewModel.cs
@@ -1,18 +1,21 @@
-using System.Threading.Tasks;
 using Catel;
 using Catel.Services;
-using Catel.Threading;
-using WolvenKit.Functionality.Services;
+using WolvenKit.Common;
 using WolvenKit.Common.Services;
-using WolvenKit.ViewModels.Shell;
-using ReactiveUI;
+using WolvenKit.Functionality.Services;
+using WolvenKit.Models;
 
 
 namespace WolvenKit.ViewModels.Editor
 {
     public class PropertiesViewModel : ToolViewModel
     {
-        #region constructors
+
+
+        private readonly ILoggerService _loggerService;
+        private readonly IMessageService _messageService;
+        private readonly IProjectManager _projectManager;
+
 
         public PropertiesViewModel(
             IProjectManager projectManager,
@@ -30,11 +33,76 @@ namespace WolvenKit.ViewModels.Editor
 
             SetupCommands();
             SetupToolDefaults();
+
+            SetToNull();
         }
 
-        #endregion constructors
 
-        #region Fields
+        /// <summary>
+        /// Initialize commands for this window.
+        /// </summary>
+        private void SetupCommands()
+        {
+        }
+
+
+
+
+
+        private void SetToNull()
+        {
+            PE_SelectedItem = null;
+            AB_SelectedItem = null;
+        }
+
+
+
+
+
+        /// <summary>
+        /// Selected Item from Project Explorer If Available.
+        /// </summary>
+        public FileModel PE_SelectedItem { get; set; }
+
+        /// <summary>
+        /// Selected Item from Asset Browser If Available.
+        /// </summary>
+        public IGameFile AB_SelectedItem { get; set; }
+
+
+
+        /// <summary>
+        /// Decides if Asset browser Selected File info should be visible.
+        /// </summary>
+        public bool AB_FileInfoVisible { get; set; }
+
+        /// <summary>
+        /// Decides if Project Explorer Selected file info should be Visible.
+        /// </summary>
+        public bool PE_FileInfoVisible { get; set; }
+
+
+        /// <summary>
+        /// Decides if Asset browser Mesh preview should be visible.
+        /// </summary>
+        public bool AB_MeshPreviewVisible { get; set; }
+
+        /// <summary>
+        /// Decides if Project Explorer Mesh preview should be visible.
+        /// </summary>
+        public bool PE_MeshPreviewVisible { get; set; }
+
+
+
+
+
+
+
+        #region ToolViewModel
+        /// <summary>
+        /// Initialize Syncfusion specific defaults that are specific to this tool window.
+        /// </summary>
+        private void SetupToolDefaults() => ContentId = ToolContentId;
 
         /// <summary>
         /// Identifies the <see ref="ContentId"/> of this tool window.
@@ -45,47 +113,8 @@ namespace WolvenKit.ViewModels.Editor
         /// Identifies the caption string used for this tool window.
         /// </summary>
         public const string ToolTitle = "Properties";
+        #endregion
 
-        private readonly ILoggerService _loggerService;
-        private readonly IMessageService _messageService;
-        private readonly IProjectManager _projectManager;
 
-        #endregion Fields
-
-        //void Importableobjects_ListChanged(object sender, ListChangedEventArgs e) => OnPropertyChanged(nameof(Importableobjects));
-
-        #region Properties
-
-        /// <summary>
-        /// Bound to the View via TreeViewBehavior.cs
-        /// </summary>
-        public ChunkViewModel SelectedChunk { get; set; }
-
-        #endregion Properties
-
-        #region Methods
-
-        public string BTitle { get; set; }
-
-        /// <summary>
-        /// Initialize commands for this window.
-        /// </summary>
-        private void SetupCommands()
-        {
-        }
-
-        /// <summary>
-        /// Initialize Avalondock specific defaults that are specific to this tool window.
-        /// </summary>
-        private void SetupToolDefaults() => ContentId = ToolContentId;
-
-        // Define a unique contentid for this toolwindow
-        //BitmapImage bi = new BitmapImage();
-        // Define an icon for this toolwindow
-        //bi.BeginInit();
-        //bi.UriSource = new Uri("pack://application:,,/Resources/Media/Images/property-blue.png");
-        //bi.EndInit();//IconSource = bi;
-
-        #endregion Methods
     }
 }
