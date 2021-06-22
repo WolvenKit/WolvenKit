419e81aed3cb1c0768c654322ea38cbc3605d743
R503B <sodanak1n@outlook.com>
2021-06-22 13:29:13 +0200

    Changes

--- WolvenKit/Views/Editor/Basic/ProjectExplorerView.xaml.cs
+++ WolvenKit/Views/Editor/Basic/ProjectExplorerView.xaml.cs
@@ -1,22 +1,5 @@
-using System;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.IO;
 using System.Windows;
-using System.Windows.Media.Imaging;
-using Catel.IoC;
-using CP77.CR2W;
-using Microsoft.WindowsAPICodePack.Dialogs;
-using Syncfusion.UI.Xaml.Grid;
-using Syncfusion.UI.Xaml.TreeGrid;
-using WolvenKit.Common;
-using WolvenKit.Common.DDS;
-using WolvenKit.Functionality.Ab4d;
-using WolvenKit.Functionality.Helpers;
-using WolvenKit.Functionality.WKitGlobal.Helpers;
-using WolvenKit.Models;
 using WolvenKit.ViewModels.Editor;
-using WolvenKit.Views.Dialogs;
 
 namespace WolvenKit.Views.Editor
 {
@@ -34,65 +17,65 @@ namespace WolvenKit.Views.Editor
             InitializeComponent();
             GlobalPEView = this;
 
-            TreeGrid.ItemsSourceChanged += TreeGrid_ItemsSourceChanged;
+            //TreeGrid.ItemsSourceChanged += TreeGrid_ItemsSourceChanged;
         }
 
-        private void TreeGrid_ItemsSourceChanged(object sender, TreeGridItemsSourceChangedEventArgs e)
-        {
-            if (TreeGrid.View != null)
-            {
-                TreeGrid.View.NodeCollectionChanged += View_NodeCollectionChanged;
-            }
-        }
+        //private void TreeGrid_ItemsSourceChanged(object sender, TreeGridItemsSourceChangedEventArgs e)
+        //{
+        //    if (TreeGrid.View != null)
+        //    {
+        //        TreeGrid.View.NodeCollectionChanged += View_NodeCollectionChanged;
+        //    }
+        //}
 
-        private void View_NodeCollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-        {
-            //if (e.NewItems != null)
-            //{
-            //    foreach (var nerd in e.NewItems)
-            //    {
-            //        Trace.WriteLine(nerd.ToString());
-            //        TreeGrid.ExpandNode((TreeNode)nerd);
-            //    }
-            //}
+        //private void View_NodeCollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        //{
+        //    //if (e.NewItems != null)
+        //    //{
+        //    //    foreach (var nerd in e.NewItems)
+        //    //    {
+        //    //        Trace.WriteLine(nerd.ToString());
+        //    //        TreeGrid.ExpandNode((TreeNode)nerd);
+        //    //    }
+        //    //}
 
-            if (ViewModel is not ProjectExplorerViewModel viewModel)
-            {
-                return;
-            }
+        //    if (ViewModel is not ProjectExplorerViewModel viewModel)
+        //    {
+        //        return;
+        //    }
 
-            var rootnodes = TreeGrid.View.Nodes.RootNodes;
-            foreach (var rootnode in rootnodes)
-            {
-                TreeGrid.ExpandNode(rootnode);
-            }
+        //    var rootnodes = TreeGrid.View.Nodes.RootNodes;
+        //    foreach (var rootnode in rootnodes)
+        //    {
+        //        TreeGrid.ExpandNode(rootnode);
+        //    }
 
 
-            Trace.WriteLine(e.Action.ToString());
-        }
+        //    Trace.WriteLine(e.Action.ToString());
+        //}
 
-        protected override void OnViewModelPropertyChanged(PropertyChangedEventArgs e)
-        {
-            if (ViewModel is not ProjectExplorerViewModel viewModel)
-            {
-                return;
-            }
+        //protected override void OnViewModelPropertyChanged(PropertyChangedEventArgs e)
+        //{
+        //    if (ViewModel is not ProjectExplorerViewModel viewModel)
+        //    {
+        //        return;
+        //    }
 
-            var name = e.PropertyName;
-            switch (name)
-            {
-                case nameof(viewModel.IsTreeBeingEdited):
-                    if (viewModel.IsTreeBeingEdited)
-                    {
-                        TreeGrid.View.BeginInit(TreeViewRefreshMode.DeferRefresh);
-                    }
-                    else
-                    {
-                        TreeGrid.View.EndInit();
-                    }
-                    break;
-            }
-        }
+        //    var name = e.PropertyName;
+        //    switch (name)
+        //    {
+        //        case nameof(viewModel.IsTreeBeingEdited):
+        //            if (viewModel.IsTreeBeingEdited)
+        //            {
+        //                TreeGrid.View.BeginInit(TreeViewRefreshMode.DeferRefresh);
+        //            }
+        //            else
+        //            {
+        //                TreeGrid.View.EndInit();
+        //            }
+        //            break;
+        //    }
+        //}
 
         #endregion Constructors
 
