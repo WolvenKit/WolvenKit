bd5578b388fc6210f8b1992140c117f8698035bd
R503B <sodanak1n@outlook.com>
2021-06-30 01:47:12 +0200

    Audio Preview Fix

--- WolvenKit.App/ViewModels/Editor/Basic/PropertiesViewModel.cs
+++ WolvenKit.App/ViewModels/Editor/Basic/PropertiesViewModel.cs
@@ -5,7 +5,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -19,11 +18,9 @@ using WolvenKit.Common;
 using WolvenKit.Common.DDS;
 using WolvenKit.Common.Extensions;
 using WolvenKit.Common.Model;
-using WolvenKit.Common.Model.Arguments;
 using WolvenKit.Common.Services;
 using WolvenKit.Functionality.Ab4d;
 using WolvenKit.Functionality.Commands;
-using WolvenKit.Functionality.Controllers;
 using WolvenKit.Functionality.Services;
 using WolvenKit.Functionality.WKitGlobal;
 using WolvenKit.Models;
@@ -222,7 +219,6 @@ namespace WolvenKit.ViewModels.Editor
             // string.Equals(model.GetExtension(), ERedExtension.bk2.ToString(), StringComparison.OrdinalIgnoreCase) ||
             if (!(string.Equals(model.GetExtension(), ERedExtension.mesh.ToString(), StringComparison.OrdinalIgnoreCase) ||
               string.Equals(model.GetExtension(), ERedExtension.wem.ToString(), StringComparison.OrdinalIgnoreCase) ||
-
               string.Equals(model.GetExtension(), ERedExtension.xbm.ToString(), StringComparison.OrdinalIgnoreCase)
               || Enum.TryParse<EUncookExtension>(PE_SelectedItem.GetExtension(), out _)
             )
@@ -415,7 +411,7 @@ namespace WolvenKit.ViewModels.Editor
 
             var arg = path.ToEscapedPath() + " -o " + outf.ToEscapedPath();
             var si = new ProcessStartInfo(
-                    Path.Combine(ISettingsManager.GetWorkDir(), "test.exe"),
+                    Path.Combine("vgmstream", "test.exe"),
                     arg
                 )
             {
