604ca51b7a608cb2d0521bd937c3b66658e4f425
rfuzzo <r.fuzzo@gmail.com>
2021-07-05 21:46:08 +0200

    fix archive packing

--- WolvenKit.App/Functionality/Controllers/Cp77Controller.cs
+++ WolvenKit.App/Functionality/Controllers/Cp77Controller.cs
@@ -178,6 +178,10 @@ namespace WolvenKit.Functionality.Controllers
             return Task.FromResult(true);
         }
 
+        /// <summary>
+        /// packs redengine files in the mod project and installs it into the game mod directory
+        /// </summary>
+        /// <returns></returns>
         public Task<bool> PackAndInstallProject()
         {
             if (_projectManager.ActiveProject is not Cp77Project cp77Proj)
@@ -186,13 +190,28 @@ namespace WolvenKit.Functionality.Controllers
                 return Task.FromResult(false);
             }
 
-            _modTools.Pack(new DirectoryInfo(cp77Proj.ModDirectory), new DirectoryInfo(cp77Proj.PackedModDirectory));
+            try
+            {
+                Directory.Delete(cp77Proj.PackedModDirectory, true);
+            }
+            catch
+            {
+
+            }
+
+            _modTools.Pack(
+                new DirectoryInfo(cp77Proj.ModDirectory),
+                new DirectoryInfo(cp77Proj.PackedModDirectory),
+                $"mod{cp77Proj.Name}");
             _loggerService.Info("Packing complete!");
 
             InstallMod();
             return Task.FromResult(true);
         }
 
+        /// <summary>
+        /// Copies the contents of the "packed" folder into the game mod directory
+        /// </summary>
         public void InstallMod()
         {
             var activeMod = _projectManager.ActiveProject;
