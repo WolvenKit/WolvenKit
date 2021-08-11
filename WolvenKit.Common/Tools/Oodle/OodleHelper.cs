d46c4d9f72540fbf7a874860b62e7083294380ce
kmirea <kim4craftart@yahoo.co.uk>
2021-08-11 02:53:07 +0200

    Inplace Buffer Handling (KARK within a KARK)

--- WolvenKit.Common/Tools/Oodle/OodleHelper.cs
+++ WolvenKit.Common/Tools/Oodle/OodleHelper.cs
@@ -265,13 +265,20 @@ namespace WolvenKit.Common.Oodle
         /// <exception cref="DecompressionException"></exception>
         public static void DecompressAndCopySegment(this Stream stream, Stream outStream, uint zSize, uint size)
         {
+            //InPlace Buffer -> KARK in a KARK
+            var oodleCompression = stream.ReadStruct<uint>();
+            if ((oodleCompression == KARK) && zSize == size)
+            {
+                size = stream.ReadStruct<uint>();
+            }
+            stream.Seek(0, SeekOrigin.Begin);
             if (zSize == size)
             {
                 stream.CopyToWithLength(outStream, (int)zSize);
             }
             else
             {
-                var oodleCompression = stream.ReadStruct<uint>();
+                oodleCompression = stream.ReadStruct<uint>();
                 if (oodleCompression == KARK)
                 {
                     var headerSize = stream.ReadStruct<uint>();
@@ -296,8 +303,35 @@ namespace WolvenKit.Common.Oodle
                         throw new DecompressionException(
                             $"Unpacked size {unpackedSize} doesn't match real size {size}.");
                     }
+                    #region INPLACE BUFFER
+                    using (MemoryStream tempStream = new MemoryStream(outputBufferSpan.ToArray()))
+                    {
+                        var oodleInner = tempStream.ReadStruct<uint>();
+                        if (oodleInner == KARK)
+                        {
+                            var innerSize = tempStream.ReadStruct<uint>();
+                            length = (int)unpackedSize - 8;
+                            var inBufferSpan = length <= SPAN_LEN
+                                ? stackalloc byte[length]
+                                : new byte[length];
+                            var outBufferSpan = innerSize <= SPAN_LEN ? stackalloc byte[(int)innerSize] : new byte[innerSize];
 
-                    outStream.Write(outputBufferSpan);
+                            stream.Read(inBufferSpan);
+
+                            long finalSize = OodleHelper.Decompress(inBufferSpan, outBufferSpan);
+                            if (finalSize != innerSize)
+                            {
+                                throw new DecompressionException(
+                                    $"Unpacked size {finalSize} doesn't match real size {innerSize}.");
+                            }
+                            outStream.Write(outBufferSpan);
+                        }
+                        else
+                        {
+                            outStream.Write(outputBufferSpan);
+                        }
+                    }
+                    #endregion
                 }
                 else
                 {
