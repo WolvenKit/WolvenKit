cc71568081bf13cb92fd2aafa7abd4f116636a60
Octavian Dima <hello@wopss.net>
2021-08-21 03:07:56 +0200

    Add support for arrays

---
+++ WolvenKit.RED4.TweakDB/Types/CArray.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WolvenKit.RED4.TweakDB.Types
+{
+    public class CArray<T> : IType
+        where T : IType
+    {
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_name))
+                {
+                    var obj = Activator.CreateInstance<T>();
+                    _name = $"array:{obj.Name}";
+                }
+
+                return _name;
+            }
+        }
+
+        public IList<T> Items = new List<T>();
+
+        public void Serialize(BinaryWriter writer)
+        {
+            writer.Write(Items.Count);
+            foreach(var item in Items)
+            {
+                item.Serialize(writer);
+            }
+        }
+    }
+}
