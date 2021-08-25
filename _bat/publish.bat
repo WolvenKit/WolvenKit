bd9909f3816a5d5db435ec36d1258bcd98f6e401
rfuzzo <r.fuzzo@gmail.com>
2021-08-25 08:47:39 +0200

    Feature/gitsubmodule (#566)
    Implemented:
    - remove legacy code, hosted elsewhere: wolvenkit 7 is updated and working here: https://github.com/rfuzzo/WolvenKit
    - add dxtex git submodules
    - cleanup slns

--- _bat/publish.bat
+++ _bat/publish.bat
@@ -4,13 +4,17 @@ cd ..\
 
 @RD /S /Q .\publish\
 
-:: @RD /S /Q .\publish\full
-:: @RD /S /Q .\publish\console
+set vs_com19="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\vsdevcmd.bat"
+call %vs_com19%
 
 echo "publish app"
-dotnet publish .\WolvenKit\WolvenKit.csproj -o publish\full --no-self-contained -r win-x64 -c Release -f net5.0-windows
+:: dotnet publish .\WolvenKit\WolvenKit.csproj -o publish\full --no-self-contained -r win-x64 -c Release -f net5.0-windows
+msbuild .\WolvenKit\WolvenKit.csproj -t:restore;Build;Publish /p:PublishDir="../publish/app/";Configuration=Release;Platform=x64;SelfContained=False;PublishReadyToRun=False;PublishTrimmed=False
+
 
 echo "publish console"
-dotnet publish .\WolvenKit.CLI\WolvenKit.CLI.csproj -o publish\console --no-self-contained -r win-x64 -c Release -f net5.0
+:: dotnet publish .\WolvenKit.CLI\WolvenKit.CLI.csproj -o publish\console --no-self-contained -r win-x64 -c Release -f net5.0
+msbuild .\WolvenKit.CLI\WolvenKit.CLI.csproj -t:restore;Build;Publish /p:PublishDir="../publish/console/";Configuration=Release;Platform=x64;SelfContained=False;PublishReadyToRun=False;PublishTrimmed=False
 
 echo "The program has completed"
+
--- release.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-cd .\_bat
-
-call fullpublish.bat
-
-echo "release files created"
-@pause
