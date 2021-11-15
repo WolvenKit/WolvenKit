62ad4fe7d1ac67755bf6c6b36d10ddf5b0e5ed76
Jack Humbert <jack.humb@gmail.com>
2021-11-14 23:05:42 -0500

    parse ints for cresource

--- WolvenKit.RED4.TweakDB/Types/CResource.cs
+++ WolvenKit.RED4.TweakDB/Types/CResource.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using RED.CRC32;
 using WolvenKit.Common.FNV1A;
+using System.Text.RegularExpressions;
 
 namespace WolvenKit.RED4.TweakDB.Types
 {
@@ -11,11 +12,22 @@ namespace WolvenKit.RED4.TweakDB.Types
         public string Text { get; set; }
         public ulong Key { get; set; }
 
-        public static implicit operator CResource(string value) => new()
+        public static implicit operator CResource(string value)
         {
-            Text = value,
-            Key = (!string.IsNullOrEmpty(value) && value != "0") ? FNV1A64HashAlgorithm.HashString(value) : 0 
-        };
+            var cres = new CResource();
+            var rx = new Regex(@"(\d+)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var m = rx.Matches(value);
+            if (m.Count == 1)
+            {
+                cres.Text = value;
+                cres.Key = Convert.ToUInt64(m[0].Groups[1].Value, 10);
+                return cres;
+            }
+
+            cres.Text = value;
+            cres.Key = (!string.IsNullOrEmpty(value) && value != "0") ? FNV1A64HashAlgorithm.HashString(value) : 0;
+            return cres;
+        }
 
         public override string ToString() => $"{Text} <raRef:CResource 0x{Key:X} / {Key}>";
         public void Serialize(BinaryWriter writer) => writer.Write(Key);
