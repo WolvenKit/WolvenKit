3fade3ba7cdca7156244867def368d57a6bdec79
Jack Humbert <jack.humb@gmail.com>
2021-12-08 12:59:19 -0500

    move fixed point additions to ext

--- WolvenKit.RED4.Types/Classes/FixedPoint.cs
+++ WolvenKit.RED4.Types/Classes/FixedPoint.cs
@@ -4,7 +4,7 @@ using static WolvenKit.RED4.Types.Enums;
 namespace WolvenKit.RED4.Types
 {
 	[REDMeta]
-	public partial class FixedPoint : RedBaseClass, IRedPrimitive<float>
+	public partial class FixedPoint : RedBaseClass
     {
 		[Ordinal(0)] 
 		[RED("Bits")] 
@@ -13,13 +13,5 @@ namespace WolvenKit.RED4.Types
 			get => GetPropertyValue<CInt32>();
 			set => SetPropertyValue<CInt32>(value);
         }
-
-        public const float FACTOR = 0.0000076293945F;
-
-        public static implicit operator FixedPoint(CFloat value) => new() { Bits = (int)((float)value / FACTOR) };
-        public static implicit operator CFloat(FixedPoint value) => (float)(value.Bits * FACTOR);
-
-        public static implicit operator FixedPoint(float value) => new() { Bits = (int)(value / FACTOR) };
-        public static implicit operator float(FixedPoint value) => value.Bits * FACTOR;
     }
 }
