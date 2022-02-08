28644fb8d7f6a6fc5bc19224d20967f0fd7443b5
Jack Humbert <jack.humb@gmail.com>
2022-02-08 13:42:01 -0500

    update req

--- WolvenKit.RED4.Archive/IO/ShaderCacheReader.cs
+++ WolvenKit.RED4.Archive/IO/ShaderCacheReader.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WolvenKit.Core.Extensions;
 using WolvenKit.RED4.Archive.Shaders;
 
 namespace WolvenKit.RED4.Archive.IO
