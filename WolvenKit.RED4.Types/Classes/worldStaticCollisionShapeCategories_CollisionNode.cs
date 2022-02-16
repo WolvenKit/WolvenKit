271ea22d76f3b1a89bb905a730aa0aaf98b778b2
seberoth <seberoth@users.noreply.github.com>
2022-02-16 15:33:39 +0100

    Added custom RTTI class Fixed array type parsing

--- WolvenKit.RED4.Types/Classes/worldStaticCollisionShapeCategories_CollisionNode.cs
+++ WolvenKit.RED4.Types/Classes/worldStaticCollisionShapeCategories_CollisionNode.cs
@@ -2,15 +2,21 @@ using static WolvenKit.RED4.Types.Enums;
 
 namespace WolvenKit.RED4.Types
 {
-	//[REDMeta]
-	//public partial class worldStaticCollisionShapeCategories_CollisionNode : RedBaseClass
-	//{
-	//	[Ordinal(0)] 
-	//	[RED("arr")] 
-	//	public [5][6]Uint16 Arr
-	//	{
-	//		get => GetPropertyValue<[5][6]Uint16>();
-	//		set => SetPropertyValue<[5][6]Uint16>(value);
-	//	}
-	//}
+	[REDMeta]
+	public partial class worldStaticCollisionShapeCategories_CollisionNode : RedBaseClass
+	{
+        // TODO: Wrong type, was lazy, should work
+        [Ordinal(0)]
+        [RED("arr", 5, 6)]
+        public CArrayFixedSize<CArrayFixedSize<CUInt16>> Arr
+        {
+            get => GetPropertyValue<CArrayFixedSize<CArrayFixedSize<CUInt16>>>();
+            set => SetPropertyValue<CArrayFixedSize<CArrayFixedSize<CUInt16>>>(value);
+        }
+
+        public worldStaticCollisionShapeCategories_CollisionNode()
+        {
+            Arr = new(5);
+        }
+    }
 }
