1d724f162eba02e923c439efb10d253ae1491849
HitmanHimself <abhin7696@gmail.com>
2022-03-12 03:13:50 +0530

    anim import tests vague

--- WolvenKit.Modkit/RED4/Tools/AnimationTools.cs
+++ WolvenKit.Modkit/RED4/Tools/AnimationTools.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System;
 using System.IO;
 using System.Linq;
 using SharpGLTF.Schema2;
@@ -9,6 +10,8 @@ using WolvenKit.Modkit.RED4.RigFile;
 using WolvenKit.RED4.Archive.CR2W;
 using WolvenKit.RED4.CR2W.Archive;
 using WolvenKit.RED4.Types;
+using WolvenKit.RED4.Archive.IO;
+using System.Text;
 
 namespace WolvenKit.Modkit.RED4
 {
@@ -56,6 +59,16 @@ namespace WolvenKit.Modkit.RED4
             var model = ModelRoot.CreateModel();
             GetAnimation(animsFile, rigFile, ref model);
 
+            /*
+            ImportAnim(ref animsFile, model);
+
+            var mms = new MemoryStream();
+            using var writer = new CR2WWriter(mms, Encoding.UTF8, true);
+            writer.WriteFile(animsFile);
+
+            File.WriteAllBytes(@"C:\Users\abhin\OneDrive\Desktop\untitled.gltf", mms.ToArray());
+            */
+
             if (isGLBinary)
             {
                 model.SaveGLB(outfile.FullName);
@@ -67,7 +80,139 @@ namespace WolvenKit.Modkit.RED4
 
             return true;
         }
+        public bool ImportAnim(ref CR2WFile animsFile, ModelRoot model)
+        {
 
+            if (animsFile == null || animsFile.RootChunk is not animAnimSet anims)
+            {
+                return false;
+            }
+
+            var setEntry = anims.Animations[0].Chunk;
+            var animAnimDes = setEntry.Animation.Chunk;
+
+            var compressed = animAnimDes.AnimBuffer.Chunk as animAnimationBufferCompressed;
+            compressed.HasRawRotations = true;
+            compressed.Duration = model.LogicalAnimations[1].Duration;
+            compressed.NumAnimKeys = 0;
+            compressed.NumConstAnimKeys = 0;
+            compressed.NumAnimKeysRaw = 0;
+            compressed.NumConstTrackKeys = 0;
+            compressed.NumFrames = Convert.ToUInt32(compressed.Duration * 30.2245898);
+            compressed.NumTrackKeys = 0;
+            compressed.NumTracks = 0;
+
+            var positions = new Dictionary<ushort, Dictionary<float, System.Numerics.Vector3>>();
+            var rotations = new Dictionary<ushort, Dictionary<float, System.Numerics.Quaternion>>();
+            var scales = new Dictionary<ushort, Dictionary<float, System.Numerics.Vector3>>();
+
+            foreach(var chan in model.LogicalAnimations[1].Channels)
+            {
+                switch (chan.TargetNodePath)
+                {
+                    case PropertyPath.translation:
+                        positions.Add((ushort)(chan.TargetNode.LogicalIndex - 1), chan.GetTranslationSampler().GetLinearKeys().ToDictionary(_=>_.Key,_=>_.Value));
+                        break;
+                    case PropertyPath.rotation:
+                        rotations.Add((ushort)(chan.TargetNode.LogicalIndex - 1), chan.GetRotationSampler().GetLinearKeys().ToDictionary(_ => _.Key, _ => _.Value));
+                        break;
+                    case PropertyPath.scale:
+                        scales.Add((ushort)(chan.TargetNode.LogicalIndex - 1), chan.GetScaleSampler().GetLinearKeys().ToDictionary(_ => _.Key, _ => _.Value));
+                        break;
+                    case PropertyPath.weights:
+                        throw new System.Exception("Morph weights channel import not supported!");
+                    default:
+                        break;
+                }
+            }
+
+            var ms = new MemoryStream();
+            var bw = new BinaryWriter(ms);
+
+            foreach(var bone in positions.Keys)
+            {
+                var dict = positions[bone];
+                foreach (var time in dict.Keys)
+                {
+                    var timeNormalized = time / model.LogicalAnimations[1].Duration;
+                    UInt16 t = Convert.ToUInt16(Math.Clamp(timeNormalized, 0, 1.0f) * UInt16.MaxValue);
+                    bw.Write(t);
+                    bw.Write(bone);
+                    var posn = new System.Numerics.Vector3(dict[time].X, -dict[time].Z, dict[time].Y);
+                    bw.Write(posn.X);
+                    bw.Write(posn.Y);
+                    bw.Write(posn.Z);
+                    compressed.NumAnimKeysRaw++;
+                }
+            }
+            foreach (var bone in rotations.Keys)
+            {
+                var dict = rotations[bone];
+                foreach (var time in dict.Keys)
+                {
+                    var timeNormalized = time / model.LogicalAnimations[1].Duration;
+                    UInt16 t = Convert.ToUInt16(Math.Clamp(timeNormalized, 0, 1.0f) * UInt16.MaxValue);
+                    bw.Write(t);
+
+
+                    var rotn = new System.Numerics.Quaternion(dict[time].X, -dict[time].Z, dict[time].Y, dict[time].W);
+
+                    UInt16 bitwise = bone;
+                    bitwise |= (UInt16)1 << 13;
+
+                    if (rotn.W < 0)
+                    {
+                        rotn.W = -rotn.W;
+                        bitwise |= (UInt16)1 << 15;
+                    }
+                    bw.Write(bitwise);
+
+                    float dotPr = 1f - rotn.W;
+
+
+                    rotn.X /= Convert.ToSingle(Math.Sqrt(2f - dotPr));
+                    rotn.Y /= Convert.ToSingle(Math.Sqrt(2f - dotPr));
+                    rotn.Z /= Convert.ToSingle(Math.Sqrt(2f - dotPr));
+
+                    var zzczxc = (rotn.X * rotn.X + rotn.Y * rotn.Y + rotn.Z * rotn.Z);
+
+                    bw.Write(rotn.X);
+                    bw.Write(rotn.Y);
+                    bw.Write(rotn.Z);
+
+                    compressed.NumAnimKeysRaw++;
+                }
+            }
+            compressed.DataAddress.ZeInBytes =(UInt32)ms.Length;
+            
+            foreach (var bone in scales.Keys)
+            {
+                var dict = scales[bone];
+                foreach (var time in dict.Keys)
+                {
+                    var timeNormalized = time / model.LogicalAnimations[1].Duration;
+                    UInt16 t = Convert.ToUInt16(Math.Clamp(timeNormalized, 0, 1.0f) * UInt16.MaxValue);
+                    bw.Write(t);
+
+                    UInt16 bitwise = bone;
+                    bitwise |= (UInt16)2 << 13;
+                    bw.Write(bitwise);
+
+                    var scal = new System.Numerics.Vector3(dict[time].X, dict[time].Y, dict[time].Z);
+
+                    bw.Write(scal.X);
+                    bw.Write(scal.Y);
+                    bw.Write(scal.Z);
+
+                    compressed.NumAnimKeysRaw++;
+                }
+            }
+            
+            anims.AnimationDataChunks[0].Buffer = new SerializationDeferredDataBuffer(ms.ToArray());
+
+            compressed.DefferedBuffer = null;
+            return true;
+        }
         public static bool GetAnimation(CR2WFile animsFile, CR2WFile rigFile, ref ModelRoot model, bool includeRig = true)
         {
 
