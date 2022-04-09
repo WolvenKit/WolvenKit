7b659929e2d9ac4ed9e0771f083f23f414588d85
seberoth <seberoth@users.noreply.github.com>
2022-04-09 13:00:21 +0200

    More properties found in archive files

--- WolvenKit.RED4.Types/ClassesExt/CameraParams.cs
+++ WolvenKit.RED4.Types/ClassesExt/CameraParams.cs
@@ -1,7 +1,26 @@
 namespace WolvenKit.RED4.Types
 {
-    public class CameraParams : RedBaseClass
+    public partial class CameraParams : RedBaseClass
     {
-        
+        [RED("contentScale")]
+        public TweakDBID ContentScale
+        {
+            get => GetPropertyValue<TweakDBID>();
+            set => SetPropertyValue<TweakDBID>(value);
+        }
+
+        [RED("cameraSkillChecks")]
+        public CHandle<EngDemoContainer> CameraSkillChecks
+        {
+            get => GetPropertyValue<CHandle<EngDemoContainer>>();
+            set => SetPropertyValue<CHandle<EngDemoContainer>>(value);
+        }
+
+        [RED("maxRotationAngle")]
+        public CFloat MaxRotationAngle
+        {
+            get => GetPropertyValue<CFloat>();
+            set => SetPropertyValue<CFloat>(value);
+        }
     }
 }
