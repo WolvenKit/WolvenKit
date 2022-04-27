998744f9d3bed63905c4d11a53452a006c7c4c94
seberoth <seberoth@users.noreply.github.com>
2022-04-27 11:48:28 +0200

    Import: Avoid trying to convert wav files, when no wav files are selected

--- WolvenKit.App/ViewModels/Tools/ImportExportViewModel.cs
+++ WolvenKit.App/ViewModels/Tools/ImportExportViewModel.cs
@@ -727,7 +727,11 @@ namespace WolvenKit.ViewModels.Tools
                             await ImportSingle(item);
                         }
                     }
-                    await ImportWavs(wavs);
+
+                    if (wavs.Count > 0)
+                    {
+                        await ImportWavs(wavs);
+                    }
                 }
                 if (IsExportsSelected)
                 {
