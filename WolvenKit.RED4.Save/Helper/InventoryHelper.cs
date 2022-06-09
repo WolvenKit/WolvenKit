ea8bc4794a310f7c4c3c42ef7dfa4ac5bcb084f6
seberoth <seberoth@users.noreply.github.com>
2022-06-09 19:15:02 +0200

    Added GetItemIdHash helper function

--- WolvenKit.RED4.Save/Helper/InventoryHelper.cs
+++ WolvenKit.RED4.Save/Helper/InventoryHelper.cs
@@ -370,4 +370,25 @@ public class InventoryHelper
         writer.Write(mod.Unknown3);
         writer.Write(mod.Unknown4);
     }
+
+    public static ulong GetItemIdHash(TweakDBID tweakDbId, uint seed, ushort unk1 = 0)
+    {
+        var c = 0xC6A4A7935BD1E995;
+
+        ulong tmp;
+
+        if (unk1 == 0)
+        {
+            tmp = seed * c;
+            tmp = (tmp >> 0x2F) ^ tmp;
+        }
+        else
+        {
+            tmp = unk1 * c;
+            tmp = ((((tmp >> 0x2F) ^ tmp) * c) ^ seed) * 0x35A98F4D286A90B9;
+            tmp = (tmp >> 0x2F) ^ tmp;
+        }
+
+        return ((tmp * c) ^ tweakDbId) * c;
+    }
 }
