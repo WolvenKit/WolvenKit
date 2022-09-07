3673fbfd6324d5be9872f9eec3c0e1f56ddd6b4f
rfuzzo <r.fuzzo@gmail.com>
2022-09-07 15:40:23 +0200

    update sound events

--- WolvenKit.Modkit/RED4/Sounds/SoundEventMetadata.cs
+++ WolvenKit.Modkit/RED4/Sounds/SoundEventMetadata.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace WolvenKit.Modkit.RED4.Sounds
 {
     public class SoundEventMetadata
     {
-        public string GameVersion { get; set; } = "1.31";
+        public string GameVersion { get; set; } = "1.6";
         public List<SoundEvent> Events { get; set; } = new();
     }
 }
