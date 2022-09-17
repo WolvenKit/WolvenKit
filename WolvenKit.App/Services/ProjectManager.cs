b10872ff652079448a28f59eb3ebe2a7d78b2d43
seberoth <seberoth@users.noreply.github.com>
2022-09-17 19:45:45 +0200

    - Fixed saving for new properties - More cleanup

--- WolvenKit.App/Services/ProjectManager.cs
+++ WolvenKit.App/Services/ProjectManager.cs
@@ -153,6 +153,8 @@ namespace WolvenKit.Functionality.Services
                         Email = obj.Email,
                         Name = obj.Name,
                         Version = obj.Version,
+                        IsRedMod = obj.IsRedMod,
+                        ExecuteDeploy = obj.ExecuteDeploy
                     }
                     : (EditorProject)null;
             }
@@ -176,7 +178,6 @@ namespace WolvenKit.Functionality.Services
                 await using var fs = new FileStream(ActiveProject.Location, FileMode.Create, FileAccess.Write);
                 var ser = new XmlSerializer(typeof(CP77Mod));
                 ser.Serialize(fs, new CP77Mod(ActiveProject));
-
             }
             catch (Exception e)
             {
@@ -203,12 +204,19 @@ namespace WolvenKit.Functionality.Services
             {
 
             }
+
             public CP77Mod(EditorProject project)
             {
                 Author = project.Author;
                 Email = project.Email;
                 Name = project.Name;
                 Version = project.Version;
+
+                if (project is Cp77Project cp77Project)
+                {
+                    IsRedMod = cp77Project.IsRedMod;
+                    ExecuteDeploy = cp77Project.ExecuteDeploy;
+                }
             }
 
             public string Author { get; set; }
@@ -218,6 +226,8 @@ namespace WolvenKit.Functionality.Services
             public string Name { get; set; }
 
             public string Version { get; set; }
+            public bool IsRedMod { get; set; }
+            public bool ExecuteDeploy { get; set; }
         }
     }
 }
