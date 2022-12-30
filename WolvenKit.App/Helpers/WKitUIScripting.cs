2db5aa15fbca4a0c2a0cced9c2fbe354a820b087
dragonzkiller <dragonzkiller@gmail.com>
2022-12-30 13:24:50 -0800

    fix: Restore ExportFiles for Scripting

--- WolvenKit.App/Helpers/WKitUIScripting.cs
+++ WolvenKit.App/Helpers/WKitUIScripting.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reactive;
 using MoreLinq;
 using Splat;
+using WolvenKit.App.ViewModels.Exporters;
 using WolvenKit.Common;
 using WolvenKit.Core.Interfaces;
 using WolvenKit.Functionality.Services;
 using WolvenKit.Modkit.Scripting;
+using WolvenKit.RED4.Archive;
 using WolvenKit.RED4.Archive.CR2W;
 using WolvenKit.RED4.Archive.IO;
-using WolvenKit.ViewModels.Shell;
 
 namespace WolvenKit.App.Helpers;
 
@@ -20,7 +20,8 @@ public class WKitUIScripting : WKitScripting
 {
     private readonly IProjectManager _projectManager;
 
-    public WKitUIScripting(ILoggerService loggerService) : base(loggerService) => _projectManager = Locator.Current.GetService<IProjectManager>();
+    public WKitUIScripting(ILoggerService loggerService)
+        : base(loggerService) => _projectManager = Locator.Current.GetService<IProjectManager>();
 
     public void SuspendFileWatcher(bool suspend)
     {
@@ -77,84 +78,84 @@ public class WKitUIScripting : WKitScripting
         }
     }
 
-    /*
-     * Todo Refactor
-    public void ExportFiles(dynamic relPaths)
+    public void ExportFiles(dynamic exportList)
     {
-        if (relPaths is null)
+        // dynamic type checking
+        // TODO: mix in hashes (V8 doesn't have a ulong equivalent though)
+        switch (exportList)
         {
-            throw new ArgumentNullException(nameof(relPaths));
-        }
-        else if (relPaths is string)
-        {
-            // since relPaths is dynamic we can support string, or string[]
-            // this converts a single string to an array (thanks JavaScript)
-            relPaths = new string[] { relPaths };
-        }
-        else
-        {
-            try
-            {
-                // this will catch exceptions with the (IList) conversion
-                // Cast<string> is lazy and  will be caught later
-                relPaths = ((IList)relPaths).Cast<string>();
-            }
-            catch (InvalidCastException ice)
-            {
-                _loggerService.Error("Unexpected data type found for argument \"relPaths\" in \"ExportFiles\".");
-                _loggerService.Error(ice.Message);
-            }
-            catch (Exception e)
-            {
-                _loggerService.Error(e);
-            }
-        }
-
-        var watcherService = Locator.Current.GetService<IWatcherService>();
-        var impExpVM = Locator.Current.GetService<AppViewModel>().ImportExportToolVM;
-
-        impExpVM.IsExportsSelected = true;
-        impExpVM.IsImportsSelected = false;
-
-        // unset every item in ExportItems
-        foreach (var expItem in impExpVM.ExportableItems)
-        {
-            expItem.IsChecked = false;
-        }
-
-        try
-        {
-            var uncookExts = Enum.GetNames(typeof(ECookedFileFormat));
-            foreach (var relPath in relPaths)
-            {
-                var filePath = Path.Combine(_projectManager.ActiveProject.ModDirectory, (string)relPath);
-                var ext = Path.GetExtension(filePath).Replace(".", "");
-                if (!File.Exists(filePath))
+            case IList list:
+                foreach(var item in list)
                 {
-                    _loggerService.Info($"{relPath} could not be found.");
+                    if (item is not string)
+                    {
+                        throw new Exception($"Unexpected datatype found for {nameof(exportList)}. Expected string or string[].");
+                    }
+                }
+                break;
+            case string exportString:
+                exportList = new string[] { exportString };
+                break;
+            case FileEntry fileEntry:
+                exportList = new string[] { fileEntry.FileName };
+                break;
+            case null:
+                throw new ArgumentNullException(nameof(exportList));
+            default:
+                throw new Exception($"Unexpected datatype found for {nameof(exportList)}. Expected string or string[].");
+        }
+
+        // get the export view model and clear the items
+        var expVM = Locator.Current.GetService<TextureExportViewModel>();
+        expVM.Items.ForEach(_ => _.IsChecked = false);
+
+        foreach (var exportItem in exportList)
+        {
+            if (exportItem is string exportPath)
+            {
+                if (exportPath.Length == 0)
+                {
                     continue;
                 }
-                if (!uncookExts.Any(e => e == ext))
+
+                // check extension is exportable
+                var uncookExts = Enum.GetNames(typeof(ECookedFileFormat));
+                var ext = Path.GetExtension(exportPath).Replace(".", "");
+                if (!uncookExts.Any(_ => _.Equals(ext, StringComparison.InvariantCultureIgnoreCase)))
                 {
-                    _loggerService.Info($"{relPath} is not an exportable CR2W file.");
+                    _loggerService.Warning($"{exportPath} does not contain a valid export extension. Skipping.");
+                    continue;
+                }
+
+                // we possibly have a relative path, so convert it to the project path
+                if (!Path.IsPathFullyQualified(exportPath))
+                {
+                    if (_projectManager.IsProjectLoaded)
+                    {
+                        exportPath = Path.Combine(_projectManager.ActiveProject.ModDirectory, exportPath);
+                    }
+                    else
+                    {
+                        _loggerService.Warning($"{exportPath} is a relative path and therefore cannot be resolved with an unloaded project. Skipping.");
+                        continue;
+                    }
+                }
+
+                if (!Path.Exists(exportPath))
+                {
+                    _loggerService.Warning($"{exportPath} could not be found. Skipping.");
                     continue;
                 }
 
                 // Set the item to be checked
-                impExpVM.ExportableItems.Where(_ => _.FullName == filePath)
+                expVM.Items.Where(_ => _.FullName.EndsWith(exportPath, StringComparison.InvariantCultureIgnoreCase))
                     .ForEach(_ => _.IsChecked = true);
             }
-            impExpVM.ProcessSelectedCommand.Execute(Unit.Default);
         }
-        catch (InvalidCastException ice)
+
+        if (expVM.Items.Any(_ => _.IsChecked))
         {
-            _loggerService.Error("Unexpected data type found for argument \"relPaths\" in \"ExportFiles\".");
-            _loggerService.Error(ice.Message);
-        }
-        catch (Exception e)
-        {
-            _loggerService.Error(e);
+            expVM.ProcessSelectedCommand.Execute(Unit.Default);
         }
     }
-    */
 }
