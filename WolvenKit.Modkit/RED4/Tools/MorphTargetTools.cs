e7f136f9ec0158eddc33bbe65ca32bd6c5ce3efa
Syrr <25871133+Syrrr@users.noreply.github.com>
2023-01-16 15:48:19 +0100

    removed unnecessary var

--- WolvenKit.Modkit/RED4/Tools/MorphTargetTools.cs
+++ WolvenKit.Modkit/RED4/Tools/MorphTargetTools.cs
@@ -143,7 +143,6 @@ namespace WolvenKit.Modkit.RED4
 
             var Names = new string[NumTargets];
             var RegionNames = new string[NumTargets];
-            var OutputNames = new string[NumTargets];
             string BaseMesh = morphBlob.BaseMesh.DepotPath;
             string BaseTexture = morphBlob.BaseTexture.DepotPath;
 
