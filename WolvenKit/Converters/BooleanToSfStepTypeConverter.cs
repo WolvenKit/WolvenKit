bbf9bdfada82881440a887c2a0dbec5f6df3f856
dragonzkiller <dragonzkiller@gmail.com>
2023-03-13 20:48:11 -0700

    Display REDmod deploy progress steps

---
+++ WolvenKit/Converters/BooleanToSfStepTypeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Syncfusion.UI.Xaml.ProgressBar;
+
+namespace WolvenKit.Converters;
+public class BooleanToSfStepTypeConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool boolVal)
+        {
+            return boolVal ? StepStatus.Indeterminate : StepStatus.Active;
+        }
+        else
+        {
+            return StepStatus.Inactive;
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
