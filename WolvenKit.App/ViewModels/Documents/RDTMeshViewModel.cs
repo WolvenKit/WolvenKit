86e9748c60dd87d7879d7445b15424d9ab30fed1
seberoth <seberoth@users.noreply.github.com>
2023-03-30 22:12:51 +0200

    fix: Mesh preview not working when duplicated material names exists. Fixes #1258

--- WolvenKit.App/ViewModels/Documents/RDTMeshViewModel.cs
+++ WolvenKit.App/ViewModels/Documents/RDTMeshViewModel.cs
@@ -309,7 +309,7 @@ public partial class RDTMeshViewModel : RedDocumentTabViewModel
             var name = GetUniqueMaterialName(me.Name.ToString().NotNull(), data);
             if (!me.IsLocalInstance)
             {
-                materials.Add(name, new Material(name));
+                materials.TryAdd(name, new Material(name));
                 continue;
             }
             CMaterialInstance? inst = null;
