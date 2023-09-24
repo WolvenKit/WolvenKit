66e7642c63792fbff279b877cfffd6983814be01
seberoth <seberoth@users.noreply.github.com>
2023-09-24 22:53:27 +0200

    Added some non-RTTI classes

---
+++ WolvenKit.RED4/Types/ClassesExt/gameuiContraPlatform.cs
@@ -0,0 +1,18 @@
+﻿namespace WolvenKit.RED4.Types;
+
+public partial class gameuiContraPlatform : inkWidgetLogicController
+{
+    [RED("image")]
+    public inkImageWidgetReference Image
+    {
+        get => GetPropertyValue<inkImageWidgetReference>();
+        set => SetPropertyValue<inkImageWidgetReference>(value);
+    }
+
+    public gameuiContraPlatform()
+    {
+        PostConstruct();
+    }
+
+    partial void PostConstruct();
+}
