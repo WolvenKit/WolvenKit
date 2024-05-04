b3d710c374ac6975f2b0adc2c0dbe111a3cdefe6
manavortex <manavortex@gmail.com>
2024-05-04 13:41:57 +0200

    handling&doku when game executable couldn't be launched

--- WolvenKit/Helpers/LogCodeHelper.cs
+++ WolvenKit/Helpers/LogCodeHelper.cs
@@ -22,6 +22,10 @@ public static class LogCodeHelper
         s_mapping.Add(0x4001, "https://wiki.redmodding.org/wolvenkit/wolvenkit-app/settings#additional-mod-directory");
         s_mapping.Add(0x4002, "https://wiki.redmodding.org/wolvenkit/wolvenkit-app/settings#game-executable-.exe-path");
         s_mapping.Add(0x4003, "https://wiki.redmodding.org/wolvenkit/wolvenkit-app/usage/wolvenkit-projects");
+
+        // 5: everything else
+        s_mapping.Add(0x5000, "https://wiki.redmodding.org/wolvenkit/wolvenkit-app/error-codes#id-0x5000-invalid-settings");
+        s_mapping.Add(0x5001, "https://wiki.redmodding.org/wolvenkit/wolvenkit-app/error-codes#id-0x5001-invalid-game-file-executable");
         
     }
 
