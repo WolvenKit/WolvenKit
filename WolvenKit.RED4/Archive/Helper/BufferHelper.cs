793a9378f199b2a5a40954aa6bd47eb6e3568379
seberoth <seberoth@users.noreply.github.com>
2024-05-28 08:08:21 +0200

    Applied `CollisionReader` to `worldFoliageDestructionNode`

--- WolvenKit.RED4/Archive/Helper/BufferHelper.cs
+++ WolvenKit.RED4/Archive/Helper/BufferHelper.cs
@@ -23,6 +23,7 @@ public static class BufferHelper
         s_bufferReaders.Add("worldSharedDataBuffer.buffer", typeof(WorldSharedDataBufferReader));
         s_bufferReaders.Add("worldStreamingSector.transforms", typeof(worldNodeDataReader));
         s_bufferReaders.Add("worldCollisionNode.compiledData", typeof(CollisionReader));
+        s_bufferReaders.Add("worldFoliageDestructionNode.compiledData", typeof(CollisionReader));
         //s_bufferReaders.Add("physicsGeometryCache.bufferTableSectors", typeof(GeometryCacheReader));
         //s_bufferReaders.Add("physicsGeometryCache.alwaysLoadedSectorDDB", typeof(GeometryCacheReader));
         s_bufferReaders.Add("CGIDataResource.data", typeof(CGIDataReader));
