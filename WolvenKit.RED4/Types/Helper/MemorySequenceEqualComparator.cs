d824e459a561723b93029882cd728bc8a0a73816
0x25CBFC4F <9310d27e@gmail.com>
2024-06-08 18:09:37 +0400

    Reworked NodeRefPool completely. Now it doesn't store parts of the paths. Implemented long forgotten alias lookup.

---
+++ WolvenKit.RED4/Types/Helper/MemorySequenceEqualComparator.cs
@@ -0,0 +1,14 @@
+﻿namespace WolvenKit.RED4.Types;
+
+public class MemorySequenceEqualComparator : IEqualityComparer<ReadOnlyMemory<char>>
+{
+    public static readonly MemorySequenceEqualComparator Instance = new();
+    
+    private MemorySequenceEqualComparator() {}
+    
+    public bool Equals(ReadOnlyMemory<char> x, ReadOnlyMemory<char> y) => 
+        x.Length == y.Length && x.Span.SequenceEqual(y.Span);
+
+    public int GetHashCode(ReadOnlyMemory<char> obj) =>
+        obj.GetHashCode();
+}
--- WolvenKit.RED4/Types/Pools/NodeRefPool.cs
+++ WolvenKit.RED4/Types/Pools/NodeRefPool.cs
@@ -1,18 +1,29 @@
-﻿using System.Buffers;
-using System.Collections.Concurrent;
-using System.Security.Cryptography;
-using System.Text;
+﻿using System.Collections.Concurrent;
 using WolvenKit.Common.FNV1A;
-using WolvenKit.RED4.Types.Helpers;
 
 namespace WolvenKit.RED4.Types;
 
 public static class NodeRefPool
 {
-    private static readonly ConcurrentDictionary<UInt128, ulong> s_pool = new();
-    private static readonly ConcurrentDictionary<ulong, StringPart> s_poolReverse = new();
+    private static readonly ConcurrentDictionary<ulong, string> s_poolReverse = new();
+    private static readonly ConcurrentDictionary<ReadOnlyMemory<char>, ReadOnlyMemory<char>> s_poolAliases = new(MemorySequenceEqualComparator.Instance);
+    
+    public static string? ResolveHash(ulong hash) => s_poolReverse.GetValueOrDefault(hash);
 
-    public static string? ResolveHash(ulong hash) => s_poolReverse.GetValueOrDefault(hash)?.Materialize();
+    public static ReadOnlyMemory<char>? ResolveAliasToMemory(ReadOnlyMemory<char> aliasName) =>
+        s_poolAliases.GetValueOrDefault(aliasName, null);
+
+    public static string? ResolveAliasToString(ReadOnlyMemory<char> aliasName)
+    {
+        var memory = ResolveAliasToMemory(aliasName);
+        return memory != null ? new string(memory.Value.Span) : null;
+    }
+
+    public static ulong? ResolveAliasToHash(ReadOnlyMemory<char> aliasName)
+    {
+        var memory = ResolveAliasToMemory(aliasName);
+        return memory != null ? FNV1A64HashAlgorithm.HashStringWithoutAliases(memory.Value.Span) : null;
+    }
 
     public static ulong AddOrGetHash(string value)
     {
@@ -20,123 +31,78 @@ public static class NodeRefPool
         {
             return 0;
         }
+
+        var hash = FNV1A64HashAlgorithm.HashStringWithoutAliases(value);
+
+        // Backwards resolving is already cached
+        if (s_poolReverse.ContainsKey(hash))
+            return hash;
+
+        CacheBackwardsHash(value, hash);
+        CacheAliases(value);
+
+        return hash;
+    }
+
+    private static void CacheBackwardsHash(string value, ulong hash) => s_poolReverse.TryAdd(hash, value);
+    
+    private static void CacheAliases(string value)
+    {
+        if (!value.Contains('#'))
+            return;
+
+        var nextAliasPos = value.IndexOf('#');
+
+        if (nextAliasPos == 0)
+            throw new InvalidOperationException("Alias shouldn't be first in the string!");
         
-        // Handling non-path strings. Those, that does not begin with $
-        if(value[0] != '$')
+        while(nextAliasPos != -1)
         {
-            return AddOrGetHashNonPath(value);
+            // We have to handle 2 cases:
+            // $/test/#important/123/456 - simple syntax
+            // $/test/important;#alias/123/456 - semicolon syntax (named alias)
+            
+            var semicolonSyntax = value[nextAliasPos - 1] == ';';
+            
+            var (aliasName, aliasPath) = !semicolonSyntax ?
+                HandleSimpleAlias(value, nextAliasPos) :
+                HandleSemicolonAlias(value, nextAliasPos);
+            
+            s_poolAliases.TryAdd(aliasName, aliasPath);
+
+            nextAliasPos = value.IndexOf('#', nextAliasPos + 1);
         }
+    }
 
-        // Since some of the path parts can contain character '#' to specify an alias -
-        // we need to remove said characters before hashing the path
-        using var hashRemover = new PathHashRemover(value);
-        var valueSpan = hashRemover.Span;
-        var valueMemory = hashRemover.Memory;
-        
-        // Checking if value was already cached
-        if (s_pool.TryGetValue(CreateStringHash(valueSpan), out var existingHash))
-        {
-            // It does exist. Returning it's hash.
-            return existingHash;
-        }
+    private static (ReadOnlyMemory<char> Name, ReadOnlyMemory<char> Path) HandleSimpleAlias(string value, int aliasPosition)
+    {
+        // Handling simple syntax
+        // $/test/#important/123/456
 
-        // Hash does not exist yet and we deal with path-looking string
-        // First we generate hash for full string
-        // $/03_night_city/#c_city_center/corpo_plaza/path/to/somewhere
-        var result = AddHash(hashRemover.Memory);
-        
-        // Now we split and generate a hash for each part of the path
-        // Like this:
-        // $/03_night_city/
-        // $/03_night_city/#c_city_center/
-        // <...>
+        var memory = value.AsMemory();
 
-        const char splitter = '/';
-        
-        var start = 0;
-        var currentSearchIndex = valueSpan.IndexOf(splitter);
+        var indexOfSlash = value.IndexOf('/', aliasPosition);
+        var nameEndPosition = indexOfSlash != -1 ? indexOfSlash : value.Length;
 
-        while (currentSearchIndex != -1)
-        {
-            // Extract the substring from the start to the current index + 1
-            var part = valueMemory[..(currentSearchIndex + 1)];
+        var name = memory[(aliasPosition+1)..nameEndPosition];
+        var path = memory[..nameEndPosition];
 
-            if (part.Length != 2)
-            {
-                // Skip hashing of $/
-                AddHash(part);
-            }
-
-            // Move the start to the current index + 1 for the next iteration
-            start = currentSearchIndex + 1;
-
-            // Find the next occurrence of the splitter
-            currentSearchIndex = valueSpan[start..].IndexOf(splitter);
-
-            if (currentSearchIndex != -1)
-            {
-                currentSearchIndex += start; // Adjust index relative to the original span
-            }
-        }
-
-        // Handle the final part after the last splitter, if any
-        if (start < valueSpan.Length)
-        {
-            AddHash(valueMemory);
-        }
-        
-        return result;
+        return (name, path);
     }
     
-    private static ulong AddHash(ReadOnlyMemory<char> value)
+    private static (ReadOnlyMemory<char> Name, ReadOnlyMemory<char> Path) HandleSemicolonAlias(string value, int aliasPosition)
     {
-        var span = value.Span;
-        var hash = FNV1A64HashAlgorithm.HashString(span, Encoding.UTF8, false, true);
+        // Handling semicolon syntax
+        // $/test/important;#alias/123/456
+
+        var memory = value.AsMemory();
         
-        s_pool.TryAdd(CreateStringHash(span), hash);
-        s_poolReverse.TryAdd(hash, new StringPart(value));
+        var indexOfSlash = value.IndexOf('/', aliasPosition);
+        var nameEndPosition = indexOfSlash != -1 ? indexOfSlash : value.Length;
 
-        return hash;
-    }
+        var name = memory[(aliasPosition+1)..nameEndPosition];
+        var path = memory[..(aliasPosition - 1)]; 
 
-    private static ulong AddOrGetHashNonPath(string value)
-    {
-        var key = CreateStringHash(value);
-        
-        if (!s_pool.TryGetValue(key, out var hash))
-        {
-            hash = FNV1A64HashAlgorithm.HashString(value, Encoding.UTF8, false, true);
-
-            s_pool.TryAdd(key, hash);
-            s_poolReverse.TryAdd(hash, new StringPart(value, 0, value.Length));
-        }
-
-        return hash;
-    }
-
-    private static unsafe UInt128 CreateStringHash(ReadOnlySpan<char> value)
-    {
-        // Two buffers, for both serialized string and generated hash
-        var strBuffer = ArrayPool<byte>.Shared.Rent(value.Length);
-        var hashBuffer = ArrayPool<byte>.Shared.Rent(16);
-        
-        try
-        {
-            // Serializing string as ASCII, since we don't actually have any UTF-8 characters in string.
-            Encoding.ASCII.GetBytes(value, strBuffer);
-
-            // Hashing serialized string
-            MD5.HashData(strBuffer, hashBuffer);
-
-            fixed (byte* hashPtr = &hashBuffer[0])
-            {
-                return *(UInt128*)&hashPtr;
-            }
-        }
-        finally
-        {
-            ArrayPool<byte>.Shared.Return(strBuffer);
-            ArrayPool<byte>.Shared.Return(hashBuffer);
-        }
+        return (name, path);
     }
 }
