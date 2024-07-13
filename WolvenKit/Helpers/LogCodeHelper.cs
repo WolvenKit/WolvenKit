1fae749891daa8f43791e034a1542e78cb767416
manavortex <manavortex@gmail.com>
2024-07-13 13:47:22 +0200

    Added types for entityNodes' data buffers

--- WolvenKit/Helpers/LogCodeHelper.cs
+++ WolvenKit/Helpers/LogCodeHelper.cs
@@ -26,6 +26,7 @@ public static class LogCodeHelper
         // 5: everything else
         s_mapping.Add(0x5000, "https://wiki.redmodding.org/wolvenkit/wolvenkit-app/error-codes#id-0x5000-invalid-settings");
         s_mapping.Add(0x5001, "https://wiki.redmodding.org/wolvenkit/wolvenkit-app/error-codes#id-0x5001-invalid-game-file-executable");
+        s_mapping.Add(0x5002, "https://github.com/WolvenKit/Wolvenkit/issues");
         
     }
 
