1a27287f105a73b293d99e22407336eedaea80d1
manavortex <manavortex@gmail.com>
2024-07-28 13:16:05 +0200

    fixed CanExecute

--- WolvenKit.App/ViewModels/Shell/AppViewModel.cs
+++ WolvenKit.App/ViewModels/Shell/AppViewModel.cs
@@ -409,7 +409,7 @@ public partial class AppViewModel : ObservableObject/*, IAppViewModel*/
     [NotifyCanExecuteChangedFor(nameof(PackInstallRedModRunCommand))]
     [NotifyCanExecuteChangedFor(nameof(ScanForBrokenReferencePathsCommand))]
     [NotifyCanExecuteChangedFor(nameof(FindUnusedFilesCommand))]
-    // [NotifyCanExecuteChangedFor(nameof(ImportFromEntitySpawnerCommand))] TODO: put this back when the other branch is merged
+    [NotifyCanExecuteChangedFor(nameof(ImportFromEntitySpawnerCommand))]
     [NotifyCanExecuteChangedFor(nameof(RunFileValidationOnProjectCommand))]
     [NotifyCanExecuteChangedFor(nameof(CheckForScriptUpdatesCommand))]
     private EStatus _taskStatus;
