ea8bb264820d54d6516586dcb07583abacf9b99e
manavortex <manavortex@gmail.com>
2024-08-04 15:24:47 +0200

    validating textbox now has a textblock

---
+++ WolvenKit/ViewModels/Validators/ItemNameValidationRule.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace WolvenKit.ViewModels.Validators;
+
+public class ItemNameValidationRule(int minLength) : ValidationRule
+{
+    public ItemNameValidationRule() : this(4)
+    {
+    }
+
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    {
+        if (value?.ToString() is not string content || string.IsNullOrWhiteSpace(content))
+        {
+            return new ValidationResult(false, "Item name cannot be empty");
+        }
+
+        if (content.Length < minLength)
+        {
+            return new ValidationResult(false, $"At least {minLength} characters");
+        }
+
+        if (content.Contains(' '))
+        {
+            return new ValidationResult(false, "No spaces allowed in item name. Use _ instead!");
+        }
+
+        if (content.Contains('.'))
+        {
+            return new ValidationResult(false, "No . allowed in item name");
+        }
+
+        return ValidationResult.ValidResult;
+    }
+}
--- WolvenKit/Views/Shell/MainView.xaml.cs
+++ WolvenKit/Views/Shell/MainView.xaml.cs
@@ -72,7 +72,6 @@ namespace WolvenKit.Views.Shell
                     return true;
                 };
 
-
                 Interactions.ShowArchiveXlFilesView = isControlFiles =>
                 {
                     AddArchiveXlFilesDialog dialog = new(isControlFiles);
