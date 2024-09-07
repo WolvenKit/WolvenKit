447dab51022f8453b020f24679a795e564bdec2e
manavortex <manavortex@gmail.com>
2024-09-07 17:17:30 +0200

    Fix/#1920 warn about capital letters and whitespaces (#1922)
    * fixed #1917
    
    * #1920 (warning about capital letters on pack)

--- WolvenKit.App/Controllers/RED4Controller.cs
+++ WolvenKit.App/Controllers/RED4Controller.cs
@@ -477,9 +477,21 @@ public class RED4Controller : ObservableObject, IGameController
         
         // copy files to packed dir
         // pack archives
-        var modfiles = Directory.EnumerateFiles(cp77Proj.ModDirectory, "*", SearchOption.AllDirectories);
+        var modfiles = Directory.EnumerateFiles(cp77Proj.ModDirectory, "*", SearchOption.AllDirectories).ToList();
         if (modfiles.Any())
         {
+            var invalidFiles = modfiles
+                .Select(f => Path.GetRelativePath(cp77Proj.ModDirectory, f))
+                .Where(f => f.Any(char.IsUpper) || f.Any(char.IsWhiteSpace)).ToList();
+            if (invalidFiles.Count != 0)
+            {
+                _loggerService.Error("Capital letters and/or whitespaces found (this may cause issues):");
+                foreach (var filePath in invalidFiles)
+                {
+                    _loggerService.Error($"\t {filePath}");
+                }
+            }
+            
             if (!await Task.Run(() => PackArchives(cp77Proj, options)))
             {
                 _progressService.IsIndeterminate = false;
