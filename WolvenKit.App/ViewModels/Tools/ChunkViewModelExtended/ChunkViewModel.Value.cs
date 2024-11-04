8bc7fb6c9a5fb1e745de803076b2132ec87be361
manavortex <manavortex@gmail.com>
2024-11-04 18:53:01 +0100

    changed display for boneParentIndices in rigs

--- WolvenKit.App/ViewModels/Tools/ChunkViewModelExtended/ChunkViewModel.Value.cs
+++ WolvenKit.App/ViewModels/Tools/ChunkViewModelExtended/ChunkViewModel.Value.cs
@@ -243,6 +243,17 @@ public partial class ChunkViewModel
                 Value = boneNames[NodeIdxInParent].GetResolvedText();
                 IsValueExtrapolated = Value != "";
                 break;
+            case CInt16 boneIdx when Parent?.Name == "boneParentIndexes" &&
+                                     GetRootModel().GetPropertyFromPath("boneNames")?.ResolvedData is CArray<CName> boneNames &&
+                                     boneNames.Count > NodeIdxInParent:
+                Value = boneNames[NodeIdxInParent].GetResolvedText();
+                IsValueExtrapolated = Value != "";
+                if (boneIdx > 0 && boneIdx < boneNames.Count)
+                {
+                    Value = $"{Value} -> {boneNames[boneIdx].GetResolvedText()}";
+                }
+
+                break;
             case scnSceneWorkspotDataId sceneWorkspotData when sceneWorkspotData.Id != 0:
                 Value = $"{sceneWorkspotData.Id}";
                 IsValueExtrapolated = sceneWorkspotData.Id != 0;
