6c73db3a8d25589fa7d8887cad43533048921382
manavortex <manavortex@gmail.com>
2024-12-17 19:47:14 +0100

    ent preview: better error msg

--- WolvenKit.App/ViewModels/Documents/RDTMeshViewModel.cs
+++ WolvenKit.App/ViewModels/Documents/RDTMeshViewModel.cs
@@ -212,7 +212,8 @@ public partial class RDTMeshViewModel : RedDocumentTabViewModel
                     }
                     catch (Exception e)
                     {
-                        _loggerService.Error($"Failed to render entity. Please open a ticket: \n${e}");
+                        _loggerService.Error(
+                            $"Failed to render entity. Please ensure that all your component names are unique. If they are, open a ticket: \n${e}");
                     }
 
                     break;
@@ -3443,7 +3444,8 @@ public partial class RDTMeshViewModel : RedDocumentTabViewModel
         }
         catch (Exception e)
         {
-            _loggerService.Error($"Failed to render entity. Please open a ticket: \n${e}");
+            _loggerService.Error(
+                $"Failed to render entity. Please ensure that all your component names are unique. If they are, open a ticket:  \n${e}");
         }
     }
 
