57af0afe0eb9d6e8043ef50a3166597bdd1e842a
Louis Poirier <contact@louispoirier.io>
2025-01-07 20:33:34 +0100

    feat(RedTreeView): add quick access to open curve editor
    - add CurveTemplate selector.
    - add icon button next to "curveData" rows to open in curve editor.

--- WolvenKit/Converters/TreeViewItemTemplateSelector.cs
+++ WolvenKit/Converters/TreeViewItemTemplateSelector.cs
@@ -10,10 +10,13 @@ namespace WolvenKit.Converters
     internal class TreeViewItemTemplateSelector : DataTemplateSelector
     {
         public DataTemplate GroupedTemplate { get; set; }
-        public DataTemplate PropertyTemplate { get; set; }
+
+        public DataTemplate CurveTemplate { get; set; }
         public DataTemplate ArrayTemplate { get; set; }
         public DataTemplate HandleTemplate { get; set; }
         public DataTemplate RefTemplate { get; set; }
+
+        public DataTemplate PropertyTemplate { get; set; }
 
         public override DataTemplate SelectTemplate(object item, DependencyObject container)
         {
@@ -22,12 +25,16 @@ namespace WolvenKit.Converters
                 return null;
             }
 
+            // NOTE: don't mess the order, it matters when resolving types below:
             return tvn.Content switch
             {
                 GroupedChunkViewModel gvm => GroupedTemplate,
+
+                ChunkViewModel vm when vm.PropertyType.IsAssignableTo(typeof(IRedLegacySingleChannelCurve)) => CurveTemplate,
                 ChunkViewModel vm when vm.PropertyType.IsAssignableTo(typeof(IList)) => ArrayTemplate,
                 ChunkViewModel vm when vm.PropertyType.IsAssignableTo(typeof(IRedHandle)) => HandleTemplate,
                 ChunkViewModel vm when vm.PropertyType.IsAssignableTo(typeof(IRedRef)) => RefTemplate,
+
                 ChunkViewModel vm => PropertyTemplate,
                 _ => null
             };
