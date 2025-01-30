67409e2530c0237cd98c87de4251afcf8d8913f9
Nawaf <40125781+Emiukabe@users.noreply.github.com>
2025-01-30 14:25:16 +0300

    early implementing ccxl xaml dialog

---
+++ WolvenKit.App/ViewModels/Dialogs/ConvertToCCXLMaterialsDialogViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DynamicData;
+
+namespace WolvenKit.App.ViewModels.Dialogs;
+public partial class ConvertToCCXLMaterialsDialogViewModel
+{
+}
---
+++ WolvenKit/Views/Dialogs/Windows/ConvertToCCXLMaterialsDialog.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WolvenKit.Views.Dialogs.Windows;
+/// <summary>
+/// Interaction logic for ConvertToCCXLMaterials.xaml
+/// </summary>
+public partial class ConvertToCCXLMaterials : Window
+{
+    public ConvertToCCXLMaterials()
+    {
+        InitializeComponent();
+    }
+}
