5c94489dad842e90226044682d8d39e52487b41d
seberoth <seberoth@users.noreply.github.com>
2025-02-09 11:32:40 +0100

    refactor(Pools): Refactored all (CName, NodeRef, ResourcePath, TweakDBID) pools

--- WolvenKit.RED4/Types/Pools/ResourcePathPool.cs
+++ WolvenKit.RED4/Types/Pools/ResourcePathPool.cs
@@ -1,79 +1,31 @@
-﻿using System.Collections.Concurrent;
-using System.Text;
+﻿using System.Text;
 using WolvenKit.Common.FNV1A;
+using WolvenKit.Core.Helpers;
 
 namespace WolvenKit.RED4.Types.Pools;
 
 public static class ResourcePathPool
 {
-    private static ConcurrentDictionary<string, ulong> s_nativePool = new();
-    private static ConcurrentDictionary<ulong, string> s_nativePoolReverse = new();
+    private static readonly BasePool s_pool;
 
-    private static readonly ConcurrentDictionary<string, ulong> s_pool = new();
-    private static readonly ConcurrentDictionary<ulong, string> s_poolReverse = new();
-
-    public static string? ResolveHash(ulong hash)
+    static ResourcePathPool()
     {
-        if (s_nativePoolReverse.TryGetValue(hash, out var value))
-        {
-            return value;
-        }
-
-        if (s_poolReverse.TryGetValue(hash, out value))
-        {
-            return value;
-        }
-
-        return ResolveHashHandler?.Invoke(hash);
+        s_pool = new BasePool(ResourcePath.SanitizePath, s => FNV1A64HashAlgorithm.HashString(s, Encoding.UTF8, false, true));
     }
 
-    public static bool IsNative(string value) => s_nativePool.ContainsKey(value);
-    public static bool IsNative(ulong value) => s_nativePoolReverse.ContainsKey(value);
+    public static string? ResolveHash(ulong hash) => s_pool.ResolveHash(hash);
 
-    public static bool IsRuntime(string value) => s_pool.ContainsKey(value);
-    public static bool IsRuntime(ulong value) => s_poolReverse.ContainsKey(value);
+    public static bool IsNative(string value) => s_pool.IsNative(value);
+    public static bool IsNative(ulong value) => s_pool.IsNative(value);
 
-    public static void AddNativeString(string value)
-    {
-        var hash = ResourcePath.CalculateHash(value);
-
-        s_nativePool.TryAdd(value, hash);
-        s_nativePoolReverse.TryAdd(hash, value);
-    }
+    public static bool IsRuntime(string value) => s_pool.IsRuntime(value);
+    public static bool IsRuntime(ulong value) => s_pool.IsRuntime(value);
 
     public static ulong AddOrGetHash(string value)
     {
-        var sanitizedValue = ResourcePath.SanitizePath(value);
-
-        if (s_nativePool.TryGetValue(sanitizedValue, out var hash))
-        {
-            return hash;
-        }
-
-        if (s_pool.TryGetValue(sanitizedValue, out hash))
-        {
-            return hash;
-        }
-
-        hash = FNV1A64HashAlgorithm.HashString(sanitizedValue, Encoding.UTF8, false, true);
-
-        s_pool.TryAdd(sanitizedValue, hash);
-        s_poolReverse.TryAdd(hash, sanitizedValue);
-
+        var (_, hash) = s_pool.AddOrGetHash(value);
         return hash;
     }
 
-    public static void SetNative(ConcurrentDictionary<ulong, string> dict)
-    {
-        s_nativePoolReverse = dict;
-        s_nativePool = new ConcurrentDictionary<string, ulong>();
-
-        foreach (var (key, value) in dict)
-        {
-            s_nativePool.GetOrAdd(value, key);
-        }
-    }
-
-    public delegate string? ExtResolveHash(ulong hash);
-    public static ExtResolveHash? ResolveHashHandler;
+    public static void SetNative(LookupTable lookupTable) => s_pool.SetNative(lookupTable);
 }
