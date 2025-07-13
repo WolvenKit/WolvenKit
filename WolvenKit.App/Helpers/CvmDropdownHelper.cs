3d9948b82dce06f32c95bc9fde1cba00adae0c80
manavortex <manavortex@gmail.com>
2025-07-13 12:55:00 +0200

    merge fixes

--- WolvenKit.App/Helpers/CvmDropdownHelper.cs
+++ WolvenKit.App/Helpers/CvmDropdownHelper.cs
@@ -128,11 +128,8 @@ public abstract class CvmDropdownHelper
             case gameJournalPath when cvm.Name is "className" && s_questHandleParentNames.Contains(parent.Name):
                 ret = RedTypeHelper.GetExtendingClassNames(typeof(gameJournalEntry));
                 break;
-            case gameJournalPath journalPath when cvm.Name is "realPath":
-                ret = documentTools.GetAllJournalPaths(forceCacheRefresh);
-                break;
-            case gameJournalPath journalPath when cvm.Name is "realPath":
-                ret = documentTools.GetAllJournalPathsAsync(forceCacheRefresh, filter).Result.Select(path => (string?)path);
+            case gameJournalPath when cvm.Name is "realPath":
+                ret = documentTools.GetAllJournalPathsAsync(forceCacheRefresh, filter).Result;
                 break;
             case entISkinTargetComponent when cvm.Name is "renderingPlaneAnimationParam":
                 ret = s_appFileRenderPlane;
