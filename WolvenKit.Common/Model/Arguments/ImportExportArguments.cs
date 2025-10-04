fdfc76b16a1283b1ec7135e3fba06a4a4ea77729
seberoth <seberoth@users.noreply.github.com>
2025-10-04 13:35:37 +0200

    feat(Import/Export): Dynamically show used args based on selections

--- WolvenKit.Common/Model/Arguments/ImportExportArguments.cs
+++ WolvenKit.Common/Model/Arguments/ImportExportArguments.cs
@@ -1,8 +1,34 @@
+using System;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace WolvenKit.Common.Model.Arguments
 {
+    /// <summary>
+    /// Tags a property as accessible in a WolvenKit Script
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class WkitScriptAccess : Attribute
+    {
+        public string ScriptName { get; }
+
+        // by default the script name is the name of the property or class
+        public WkitScriptAccess([CallerMemberName] string scriptName = "") => ScriptName = scriptName;
+    }
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class UsedWith : Attribute
+    {
+        public string PropertyName { get; }
+        public object?[] Values { get; }
+
+        public UsedWith(string propertyName, params object?[] values)
+        {
+            PropertyName = propertyName;
+            Values = values;
+        }
+    }
 
     /// <summary>
     /// Import Export Arguments
@@ -11,6 +37,4 @@ namespace WolvenKit.Common.Model.Arguments
     {
         [Browsable(false)] public static bool IsCLI { get; set; } = false;
     }
-
-
 }
