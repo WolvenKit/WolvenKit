ec1658d6df26f8c0989ddfc29241713253b88292
manavortex <manavortex@gmail.com>
2025-10-23 18:50:08 +0200

    Inkatlas generator: improvements (#2723)
    * WIP
    
    * better inkatlas creation dialogue

--- WolvenKit.App/ViewModels/Dialogs/AddInkatlasDialogViewModel.cs
+++ WolvenKit.App/ViewModels/Dialogs/AddInkatlasDialogViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
 using WolvenKit.App.Models.ProjectManagement.Project;
@@ -13,31 +14,58 @@ public partial class AddInkatlasDialogViewModel : ObservableObject
     {
         // Dropdown menu should have all folders in mod directory available
         ProjectFolders = project.GetAllFolders(project.ModDirectory).ToDictionary<string, string>(x => x);
-
+        ExistingFiles = project.ModFiles.Where(f => f.HasFileExtension(".inkatlas"))
+            .ToDictionary<string, string>(x => x);
         InkatlasFileName = $"{project.Name}_icons";
+
+        IconFileSizes = new Dictionary<string, (int, int)>()
+        {
+            { "Clothing item (160x160)", (160, 160) },
+            { "Weapon small (160x80)", (160, 80) },
+            { "Weapon (160x120)", (160, 80) },
+            { "NPV", (200, 200) },
+        };
     }
 
     protected override void OnPropertyChanged(PropertyChangedEventArgs e)
     {
         switch (e.PropertyName)
         {
+            case nameof(InkatlasFileName):
+                InkatlasFileName = InkatlasFileName.ToFileName().Replace(".inkatlas", "");
+                break;
             case nameof(RelativePath):
                 RelativePath = RelativePath.ToFilePath();
                 break;
-            case nameof(InkatlasFileName):
-                InkatlasFileName = InkatlasFileName.ToFileName().Replace(".inkatlas", "");
+            case nameof(ExistingFile) when !string.IsNullOrEmpty(ExistingFile):
+                // No need to escape, the propertyChangedEvents will do that
+                if (Path.GetDirectoryName(ExistingFile) is string path)
+                {
+                    RelativePath = path;
+                    InkatlasFileName = Path.GetFileNameWithoutExtension(ExistingFile);
+                }
+
                 break;
         }
 
         base.OnPropertyChanged(e);
     }
 
+    [ObservableProperty] private Dictionary<string, string> _existingFiles;
+
     [ObservableProperty] private Dictionary<string, string> _projectFolders;
+
+    [ObservableProperty] private Dictionary<string, (int, int)> _iconFileSizes;
 
     /// <summary>
     /// Remember the last selection
     /// </summary>
     [ObservableProperty] private bool _rememberValues = true;
+
+    /// <summary>
+    /// Relative path to inkatlas folder
+    /// </summary>
+    [ObservableProperty] private string _existingFile = "";
 
     /// <summary>
     /// Relative path to inkatlas folder
