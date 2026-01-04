e50e2e647ed7b79ab291216efd408811a4d93eee
manavortex <manavortex@gmail.com>
2026-01-04 15:45:43 +0100

    copy constructors and IRedMeshNode interface

--- WolvenKit.RED4/Types/ClassesExt/Vector4.cs
+++ WolvenKit.RED4/Types/ClassesExt/Vector4.cs
@@ -8,5 +8,13 @@ public partial class Vector4
     public static implicit operator System.Numerics.Vector4(Vector4 v) =>
         new System.Numerics.Vector4 { X = v.X, Y = v.Y, Z = v.Z, W = v.W };
 
+    public Vector4(Vector4 other)
+    {
+        X = other.X;
+        Y = other.Y;
+        Z = other.Z;
+        W = other.W;
+    }
+
     public override string ToString() => $"Vector4, X = {X}, Y = {Y}, Z = {Z}, W = {W}";
 }
--- WolvenKit.RED4/Types/Interfaces/IRedMeshNode.cs
+++ WolvenKit.RED4/Types/Interfaces/IRedMeshNode.cs
@@ -6,54 +6,19 @@ public interface IRedMeshNode
 
     public CName MeshAppearance { get; set; }
 
-}
 
-public partial class worldBendedMeshNode : IRedMeshNode
-{
-
-}
-
-public partial class worldStaticMeshNode : IRedMeshNode
-{
-
-}
-
-public partial class worldInstancedMeshNode : IRedMeshNode
-{
-
-}
-
-public partial class worldPrefabProxyMeshNode : IRedMeshNode
-{
-
-}
-
-public partial class worldFoliageNode : IRedMeshNode
-{
 
 }
 
 public partial class worldMeshNode : IRedMeshNode
 {
-
 }
-
-/*public partial class worldStaticOccluderMeshNode : IRedMeshNode
+public partial class worldBendedMeshNode : IRedMeshNode
 {
-
-    [REDProperty(IsIgnored = true)]
-    [Ordinal(9)]
-    [RED("MeshAppearance")]
-    public CName MeshAppearance
-    {
-        get
-        {
-            return "default";
-        }
-        set
-        {
-
-        }
-    }
-
-}*/
+}
+public partial class worldInstancedMeshNode : IRedMeshNode
+{
+}
+public partial class worldFoliageNode : IRedMeshNode
+{
+}
