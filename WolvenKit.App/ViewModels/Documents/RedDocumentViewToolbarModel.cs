4415fa11efb3d21d98c044b2431bb2384024cdfd
manavortex <manavortex@gmail.com>
2026-01-04 15:46:17 +0100

    added menu items

--- WolvenKit.App/ViewModels/Documents/RedDocumentViewToolbarModel.cs
+++ WolvenKit.App/ViewModels/Documents/RedDocumentViewToolbarModel.cs
@@ -35,6 +35,9 @@ public partial class RedDocumentViewToolbarModel : ObservableObject
     private readonly CRUIDService _cruidService;
     private readonly DocumentTools _documentTools;
     private readonly ILoggerService _loggerService;
+    private readonly INotificationService _notificationService;
+    private readonly StreamingSectorTools _sectorTools;
+    private readonly AppViewModel _appViewModel;
 
     public RedDocumentViewToolbarModel(
         ISettingsManager settingsManager,
@@ -42,7 +45,10 @@ public partial class RedDocumentViewToolbarModel : ObservableObject
         IProjectManager projectManager,
         DocumentTools documentTools,
         CRUIDService cruidService,
-        ILoggerService loggerService
+        ILoggerService loggerService,
+        INotificationService notificationService,
+        StreamingSectorTools sectorTools,
+        AppViewModel appViewModel
     )
     {
         _modifierViewStateService = modifierSvc;
@@ -51,6 +57,9 @@ public partial class RedDocumentViewToolbarModel : ObservableObject
         _cruidService = cruidService;
         _documentTools = documentTools;
         _loggerService = loggerService;
+        _sectorTools = sectorTools;
+        _notificationService = notificationService;
+        _appViewModel = appViewModel;
 
         modifierSvc.ModifierStateChanged += OnModifierChanged;
         modifierSvc.PropertyChanged += (_, args) => OnPropertyChanged(args.PropertyName);
@@ -106,11 +115,12 @@ public partial class RedDocumentViewToolbarModel : ObservableObject
                 or RedDocumentItemType.Questphase
                 or RedDocumentItemType.Scene
                 or RedDocumentItemType.Physmatlib
+                or RedDocumentItemType.Streamingsector
+                or RedDocumentItemType.Streamingblock
+                or RedDocumentItemType.Mlsetup
                 or RedDocumentItemType.Ent => true,
             RedDocumentItemType.Xbm
                 or RedDocumentItemType.Mlmask
-                or RedDocumentItemType.Mlsetup
-                or RedDocumentItemType.Sector
                 or RedDocumentItemType.None
                 or RedDocumentItemType.Other => false,
             _ => false,
@@ -188,6 +198,7 @@ public partial class RedDocumentViewToolbarModel : ObservableObject
     [NotifyCanExecuteChangedFor(nameof(RegenerateIdsCommand))]
     [NotifyCanExecuteChangedFor(nameof(CopyMaterialFromMeshCommand))]
     [NotifyCanExecuteChangedFor(nameof(CopyMaterialToMeshesCommand))]
+    [NotifyCanExecuteChangedFor(nameof(AddSectorVariantCommand))]
     [ObservableProperty]
     private RedDocumentItemType _contentType;
 
@@ -737,7 +748,7 @@ public partial class RedDocumentViewToolbarModel : ObservableObject
     #region phymatlibFile
 
     /*
-     * Delete duplicate entries
+     * Regenerate IDs
      */
     private bool CanRegenerateIds() => ContentType is RedDocumentItemType.Physmatlib;
 
@@ -746,6 +757,47 @@ public partial class RedDocumentViewToolbarModel : ObservableObject
 
     #endregion
 
+    #region sector
+
+    /*
+     * Create sector variant(s) in streamingblock
+     */
+    private bool CanCreateVariants() => ContentType is RedDocumentItemType.Streamingblock;
+
+    [RelayCommand(CanExecute = nameof(CanCreateVariants))]
+    private void AddSectorVariant()
+    {
+        if (RootChunk?.ResolvedData is not worldStreamingBlock block ||
+            _projectManager.ActiveProject is not { } project)
+        {
+            return;
+        }
+
+        if (RootChunk.Tab?.Parent.IsDirty == true)
+        {
+            Interactions.ShowErrorPopup(("Save your file first!",
+                "Your file has un-saved changes. Please save before running this workflow."));
+            return;
+        }
+
+        try
+        {
+            if (_sectorTools.AddSectorVariants(block, project))
+            {
+                RootChunk.Tab?.Parent.SetIsDirty(true);
+                RootChunk.GetPropertyChild("descriptors")?.RecalculateProperties();
+                _appViewModel.ReloadChangedFiles();
+            }
+        }
+        catch (Exception e)
+        {
+            _notificationService.Error(e.Message);
+            _loggerService.Error(e.Message);
+            return;
+        }
+    }
+
+    #endregion
 
     private bool CanDeleteChunkMaterialByIndex() => RootChunk?.ResolvedData is CMesh mesh && mesh.Appearances.Count > 0;
 
