f49cddbbe4fee12a48facb2066e2a6a761acf90a
manavortex <manavortex@gmail.com>
2026-04-16 17:25:20 +0200

    user feedback

--- WolvenKit/Views/Documents/RedDocumentViewMenuBar.xaml.cs
+++ WolvenKit/Views/Documents/RedDocumentViewMenuBar.xaml.cs
@@ -397,12 +397,14 @@ namespace WolvenKit.Views.Documents
         {
             _cvmTools.UnDynamifyMaterials(cvm);
             ViewModel?.DeleteUnusedMaterialsCommand?.NotifyCanExecuteChanged();
+            _notificationService.Success("Dynamic materials resolved");
         }
 
         private void ExpandMeshAppearances(ChunkViewModel? cvm)
         {
             _cvmTools.ExpandMeshAppearances(cvm, out var _, true);
             ViewModel?.DeleteUnusedMaterialsCommand?.NotifyCanExecuteChanged();
+            _notificationService.Success("Mesh appearances expanded");
         }
 
         private void OnUnDynamifyMaterialsClick(object _, RoutedEventArgs e) => UnDynamifyMaterials(RootChunk);
