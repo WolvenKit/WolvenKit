67f8427995d578f498064bb8f0f2a0bc1f3e2422
Vitamin Arrr <gistya@gmail.com>
2026-07-28 18:18:13 -0700

    Clean up unintended indents

--- WolvenKit.App/Models/FileSystemModel.cs
+++ WolvenKit.App/Models/FileSystemModel.cs
@@ -235,10 +235,10 @@ public class FileSystemModel : INotifyPropertyChanged
 
     public event PropertyChangedEventHandler? PropertyChanged;
 
-     protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
-         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
-     private bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    private bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
     {
         if (EqualityComparer<T>.Default.Equals(field, value))
         {
