28d533a6d6c5e5667fc4d82276868fb41660120b
Vitamin Arrr <gistya@gmail.com>
2026-08-03 15:45:30 -0700

    Load archive manager at launch

--- WolvenKit/GenericHost.cs
+++ WolvenKit/GenericHost.cs
@@ -100,6 +100,7 @@ namespace WolvenKit
                     services.AddSingleton<IModifierViewStateService, ModifierViewStateService>();
                     services.AddSingleton<INodeSelectionService, NodeSelectionService>();
                     services.AddSingleton<IWatcherService, WatcherService>();
+                    services.AddTransient<IArchiveManagerLoader, ArchiveManagerLoader>();
 
                     // factories
                     services.AddTransient<IPageViewModelFactory, PageViewModelFactory>();
