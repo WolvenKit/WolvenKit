f44540c05bcaf4a53e21275652397f590374ab73
Jonathan Gilbert <github@jongilbert.com>
2026-08-04 12:53:19 -0700

    Watcher: add BatchUpdate

--- WolvenKit.App/Models/DispatchedObservableCollection.cs
+++ WolvenKit.App/Models/DispatchedObservableCollection.cs
@@ -8,4 +8,20 @@ public class DispatchedObservableCollection<T> : ObservableCollection<T>
     public new void Add(T item) => DispatcherHelper.RunOnMainThread(() => base.Add(item));
     public new void Remove(T item) => DispatcherHelper.RunOnMainThread(() => base.Remove(item));
     public new void Clear() => DispatcherHelper.RunOnMainThread(() => base.Clear());
+
+    public void AddRange(IEnumerable<T> items)
+    {
+        var itemsList = items as IList<T> ?? items.ToList();
+
+        if (itemsList.Count == 0)
+        {
+            return;
+        }
+
+        DispatcherHelper.RunOnMainThread(() =>
+        {
+            Items.AddRange(itemsList);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        });
+    }
 }
