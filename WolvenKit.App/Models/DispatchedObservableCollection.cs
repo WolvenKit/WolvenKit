1d164e2be34a4190ecf4ce58c817a3f6f6af8ffe
Vitamin Arrr <gistya@gmail.com>
2026-08-04 16:20:37 -0700

    Cleanup rebase issues

--- WolvenKit.App/Models/DispatchedObservableCollection.cs
+++ WolvenKit.App/Models/DispatchedObservableCollection.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using DynamicData;
 using WolvenKit.App.Helpers;
 
 namespace WolvenKit.App.Models;
