c9dcf3a361d03897b695011e40703926b95538cb
Vitamin Arrr <gistya@gmail.com>
2026-08-14 07:52:01 -0700

    Make WatcherService part of ProjectExplorerViewModel

--- WolvenKit/Views/Documents/RedDocumentViewMenuBar.xaml.cs
+++ WolvenKit/Views/Documents/RedDocumentViewMenuBar.xaml.cs
@@ -45,7 +45,6 @@ namespace WolvenKit.Views.Documents
         private readonly IProjectManager _projectManager;
         private readonly ILoggerService _loggerService;
         private readonly INotificationService _notificationService;
-        private readonly WatcherService _projectWatcher;
         private readonly IAppArchiveManager _archiveManager;
         private readonly IModifierViewStateService _modifierStateService;
         private readonly ProjectExplorerViewModel _projectExplorer;
@@ -56,24 +55,21 @@ namespace WolvenKit.Views.Documents
         private readonly ICvmTools _cvmTools;
         private readonly Cr2WTools _cr2WTools;
 
-
         public RedDocumentViewMenuBar()
         {
             _scriptService = Locator.Current.GetService<AppScriptService>()!;
             _settingsManager = Locator.Current.GetService<ISettingsManager>()!;
             _projectManager = Locator.Current.GetService<IProjectManager>()!;
             _loggerService = Locator.Current.GetService<ILoggerService>()!;
-            _projectWatcher = (WatcherService)Locator.Current.GetService<IWatcherService>()!;
             _archiveManager = Locator.Current.GetService<IAppArchiveManager>()!;
             _modifierStateService = Locator.Current.GetService<IModifierViewStateService>()!;
             _progressService = Locator.Current.GetService<IProgressService<double>>()!;
-            _projectExplorer = Locator.Current.GetService<AppViewModel>()!.GetToolViewModel<ProjectExplorerViewModel>();
             _projectResourceTools = Locator.Current.GetService<ProjectResourceTools>()!;
             _cr2WTools = Locator.Current.GetService<Cr2WTools>()!;
             _notificationService = Locator.Current.GetService<INotificationService>()!;
             _cvmTools = Locator.Current.GetService<ICvmTools>()!;
-
             _appViewModel = Locator.Current.GetService<AppViewModel>()!;
+            _projectExplorer = _appViewModel.GetToolViewModel<ProjectExplorerViewModel>()!;
 
             // Enforce instance generation and service injection. One would assume that registering a singleton
             // is enough. One would be wrong.
@@ -872,7 +868,7 @@ namespace WolvenKit.Views.Documents
                     return;
                 }
 
-                _projectWatcher.Suspend();
+                _projectExplorer.Suspend();
 
                 await AddDependenciesToFileAsync(cvm, eventArgs is AddDependenciesFullEventArgs);
                 _notificationService.Success("Successfully added dependencies");
@@ -899,11 +895,10 @@ namespace WolvenKit.Views.Documents
                 await Task.Run(async () =>
                 {
                     await Task.Delay(100);
-                    _projectWatcher.Refresh();
-                    _projectWatcher.Resume();
+                    _projectExplorer.RefreshWatcher();
+                    _projectExplorer.Resume();
                 });
             }
-
         }
 
         private List<appearanceAppearanceDefinition> GetAppearancesFromSelectionOrRoot()
